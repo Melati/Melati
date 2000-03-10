c9390e995bb3fab572a7a468ad6ffef368f2aed8
William Chesters <williamc@paneris.org>
2000-03-10 15:17:30 +0000

    Add standard constructor

--- melati/src/main/java/org/melati/util/VersionVector.java
+++ melati/src/main/java/org/melati/util/VersionVector.java
@@ -1,6 +1,13 @@
 package org.melati.util;
 
 public class VersionVector extends java.util.Vector implements Version {
+
+  public VersionVector(int initialCapacity) {
+    super(initialCapacity);
+  }
+
+  public VersionVector() {}
+
   public Object clone() {
     return super.clone();
   }

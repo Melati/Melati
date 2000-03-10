aac7c38990424787555c92e1cfb78f5591fec2aa
William Chesters <williamc@paneris.org>
2000-03-10 15:16:41 +0000

    Add comment about password visibility

--- poem/src/main/java/org/melati/poem/User.java
+++ poem/src/main/java/org/melati/poem/User.java
@@ -4,11 +4,6 @@ import java.util.*;
 import java.sql.*;
 import org.melati.util.*;
 
-// the simplest way to invalidate the user/capability cache when
-// things change is to erase them ALL whenever anything significant
-// changes---which is bound to be rare
-// but for that we need the operation "zap all currently cached Users"
-
 public class User extends UserBase implements AccessToken {
   public boolean givesCapability(Capability capability) {
     return getDatabase().hasCapability(this, capability);

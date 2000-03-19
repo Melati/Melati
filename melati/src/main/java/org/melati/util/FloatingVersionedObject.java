f44a1e54271a6928b6838a8bd5b79b5e477bdd6d
William Chesters <williamc@paneris.org>
2000-03-19 20:31:59 +0000

    Add a few comments

--- melati/src/main/java/org/melati/util/FloatingVersionedObject.java
+++ melati/src/main/java/org/melati/util/FloatingVersionedObject.java
@@ -6,7 +6,24 @@ public abstract class FloatingVersionedObject extends AbstractVersionedObject {
 
   protected abstract int sessionsMax();
 
+  /**
+   * Dummy method to satisfy <TT>CacheNode</TT> (want multiple inheritance
+   * really).
+   */
+
   public Object getKey() { return null; }
+
+  /**
+   * Dummy method to satisfy <TT>CacheNode</TT> (want multiple inheritance
+   * really).
+   */
+
   public void delete(Session session) {}
+
+  /**
+   * Dummy method to satisfy <TT>CacheNode</TT> (want multiple inheritance
+   * really).
+   */
+
   public void writeDown(Session session, Version version) {}
 }

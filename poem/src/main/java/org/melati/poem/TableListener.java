2c56c086330eae8840435685d757dbb0773e9161
William Chesters <williamc@paneris.org>
2000-03-19 20:36:54 +0000

    Add notification of explicit cache invalidation

--- poem/src/main/java/org/melati/poem/TableListener.java
+++ poem/src/main/java/org/melati/poem/TableListener.java
@@ -3,4 +3,5 @@ package org.melati.poem;
 public interface TableListener {
   void notifyTouched(PoemSession session, Table table, Integer troid,
                      Data data);
+  void notifyUncached(Table table);
 }

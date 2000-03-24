b1cdfc33b8bbbe0f9ef78ba33c68c8963e62a63f
William Chesters <williamc@paneris.org>
2000-03-24 09:13:30 +0000

    Add bug-describing argument

--- poem/src/main/java/org/melati/poem/PoemBugPoemException.java
+++ poem/src/main/java/org/melati/poem/PoemBugPoemException.java
@@ -1,7 +1,8 @@
 package org.melati.poem;
 
 public class PoemBugPoemException extends SeriousPoemException {
-  String bug;
+
+  public String bug;
 
   public PoemBugPoemException(String bug) {
     this.bug = bug;

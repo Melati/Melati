52c3b8f4025935acfdc47613ad9d07a911379e62
Tim Joyce <timj@paneris.org>
2000-04-19 16:07:43 +0000

    fix compile errors

--- melati/src/main/java/org/melati/test/Regression.java
+++ melati/src/main/java/org/melati/test/Regression.java
@@ -7,14 +7,15 @@ public class Regression {
   public static final String dbName = "melatiregression";
 
   public static void main(String[] args) throws Exception {
-    if (Runtime.exec("destroydb " + dbName).waitFor() != 0 ||
-	Runtime.exec("createdb " + dbName).waitFor() != 0)
-      exit(1);
+    // ttj remove to allow it to compile
+//    if (Runtime.exec("destroydb " + dbName).waitFor() != 0 ||
+//	     Runtime.exec("createdb " + dbName).waitFor() != 0)
+//      exit(1);
 
     final Database database = new PoemDatabase();
 
     database.connect(new org.melati.poem.postgresql.jdbc2.Driver(),
-		     "jdbc:postgresql:" + melatiregression, "postgres", "*");
+		     "jdbc:postgresql:" + dbName, "postgres", "*");
 
     // to test:
 

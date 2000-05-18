a7434b403e64ee28096036aec0572d2170f804a3
Tim Joyce <timj@paneris.org>
2000-05-18 14:19:47 +0000

    system properties table

---
+++ poem/src/main/java/org/melati/poem/SystemProperties.java
@@ -0,0 +1,11 @@
+package org.melati.poem;
+
+import java.util.*;
+import java.sql.Date;
+import org.melati.util.*;
+
+public class SystemProperties extends SystemPropertiesBase {
+  public SystemProperties() {}
+
+  // programmer's domain-specific code here
+}
---
+++ poem/src/main/java/org/melati/poem/SystemPropertiesTable.java
@@ -0,0 +1,16 @@
+package org.melati.poem;
+
+import java.util.*;
+import java.sql.Date;
+import org.melati.util.*;
+
+public class SystemPropertiesTable extends SystemPropertiesTableBase {
+
+  public SystemPropertiesTable(
+      Database database, String name,
+      DefinitionSource definitionSource) throws PoemException {
+    super(database, name, definitionSource);
+  }
+
+  // programmer's domain-specific code here
+}

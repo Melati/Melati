e2c6cb1a02999f72f3780469eb7feb59e363f51a
William Chesters <williamc@paneris.org>
2000-05-19 08:31:21 +0000

    Add cache for by-name lookups

--- poem/src/main/java/org/melati/poem/SystemPropertiesTable.java
+++ poem/src/main/java/org/melati/poem/SystemPropertiesTable.java
@@ -6,11 +6,37 @@ import org.melati.util.*;
 
 public class SystemPropertiesTable extends SystemPropertiesTableBase {
 
+  private static final Object nullEntry = new Object();
+
   public SystemPropertiesTable(
       Database database, String name,
       DefinitionSource definitionSource) throws PoemException {
     super(database, name, definitionSource);
   }
 
-  // programmer's domain-specific code here
+  private Hashtable cache = null;
+  private long cacheSerial = 0L;
+
+  public String get(String name) {
+    if (cache == null || cacheSerial != serial(PoemThread.transaction()))
+      cache = new Hashtable();
+    Object value = cache.get(name);
+    if (value == nullEntry)
+      return null;
+    else if (value != null)
+      return (String)value;
+    else {
+      SystemProperties prop =
+          (SystemProperties)getNameColumn().firstWhereEq(name);
+      if (prop == null) {
+	cache.put(name, nullEntry);
+	return null;
+      }
+      else {
+	String propValue = prop.getValue();
+	cache.put(name, propValue == null ? nullEntry : propValue);
+	return propValue;
+      }
+    }
+  }
 }

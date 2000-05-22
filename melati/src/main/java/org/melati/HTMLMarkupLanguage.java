42cf37c02b096236077b5beba746ecdf00f05764
William Chesters <williamc@paneris.org>
2000-05-22 17:39:41 +0000

    Move "rendered" into HTMLUtil

--- melati/src/main/java/org/melati/HTMLMarkupLanguage.java
+++ melati/src/main/java/org/melati/HTMLMarkupLanguage.java
@@ -73,38 +73,8 @@ public class HTMLMarkupLanguage extends MarkupLanguage {
     return attributeML;
   }
 
-  public final String elementFor(char c) {
-    switch (c) {
-      case '<': return "&lt;";
-      case '>': return "&gt;";
-      case '&': return "&amp;";
-      case '"': return "&quot;";
-      default: return null;
-    }
-  } 
-
   public String rendered(String s) {
-    int l = s.length();
-    int i = 0;
-    String element = null;
-    for (i = 0; i < l && (element = elementFor(s.charAt(i))) == null; ++i);
-
-    if (element == null) return s;
-
-    StringBuffer b = new StringBuffer(l * 2);
-    for (int j = 0; j < i; ++j)
-      b.append(s.charAt(j));
-
-    b.append(element);
-
-    char c;
-    for (++i; i < l; ++i)
-      if ((element = elementFor(c = s.charAt(i))) != null)
-        b.append(element);
-      else
-        b.append(c);
-
-    return b.toString();
+    return HTMLUtils.entitied(s);
   }
 
   public String escaped(String s) {

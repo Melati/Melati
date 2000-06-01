8be90fe19c2f1f018ec12c0316f9bc2cc3ddc8e6
William Chesters <williamc@paneris.org>
2000-06-01 12:21:38 +0000

    Add a constructor with just a table, for nonexistent but table-tied objects (of questionable value actually, should use table.newPersistent)

--- poem/src/main/java/org/melati/poem/Persistent.java
+++ poem/src/main/java/org/melati/poem/Persistent.java
@@ -69,6 +69,10 @@ public class Persistent extends Transactioned implements Cloneable {
     this.troid = troid;
   }
 
+  public Persistent(Table table) {
+    this.table = table;
+  }
+
   public Persistent() {
   }
 

3f8ae12bf2c1f83b160797f0917df487820c383d
William Chesters <williamc@paneris.org>
2000-06-14 17:45:16 +0000

    *** empty log message ***

---
+++ melati/src/main/java/org/melati/util/ThrowingPrintWriter.java
@@ -0,0 +1,80 @@
+package org.melati.util;
+
+import java.io.*;
+
+public final class ThrowingPrintWriter extends PrintWriter {
+  private PrintWriter pw;
+  public final String description;
+
+  public static class SuperUseException extends BugException {
+    public SuperUseException() {
+      super("ThrowingPrintWriter tried to use super.out");
+    }
+  }
+
+  private static class ForbiddenWriter extends Writer {
+    public void write(char[] buf, int off, int len) {
+      throw new SuperUseException();
+    }
+
+    public void flush() {
+      throw new SuperUseException();
+    }
+
+    public void close() {
+      throw new SuperUseException();
+    }
+  }
+
+  public ThrowingPrintWriter(PrintWriter pw, String description) {
+    super(pw);
+    this.pw = pw;
+    this.description = description;
+  }
+
+  public class TroubleException extends MelatiRuntimeException {
+    public String getMessage() {
+      return "An exception condition occurred writing to " +
+             (description == null ? "a PrintWriter" : description);
+    }
+  }
+
+  public void throwOnTrouble() {
+    if (pw.checkError())
+      throw new TroubleException();
+  }
+
+  public void flush() {
+    pw.flush();
+    throwOnTrouble();
+  }
+
+  public void close() {
+    pw.close();
+    throwOnTrouble();
+  }
+
+  public boolean checkError() {
+    return pw.checkError();
+  }
+
+  public void write(int c) {
+    pw.write(c);
+    throwOnTrouble();
+  }
+
+  public void write(char buf[], int off, int len) {
+    pw.write(buf, off, len);
+    throwOnTrouble();
+  }
+
+  public void write(String buf, int off, int len) {
+    pw.write(buf, off, len);
+    throwOnTrouble();
+  }
+
+  public void println() {
+    pw.println();
+    throwOnTrouble();
+  }
+}

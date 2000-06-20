ef339d1376d6171213bcbbda1948433d406e8c2f
Dave Warnock <davew@paneris.org>
2000-06-20 18:07:30 +0000

    Oops lost custom versions, re-instating (thanks Erik)

--- poem/src/main/java/org/melati/poem/Setting.java
+++ poem/src/main/java/org/melati/poem/Setting.java
@@ -1,12 +1,198 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * -------------------------------------
+ *  Copyright (C) 2000 William Chesters
+ * -------------------------------------
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Contact details for copyright holder:
+ *
+ *     William Chesters <williamc@paneris.org>
+ *     http://paneris.org/~williamc
+ *     Obrechtstraat 114, 2517VX Den Haag, The Netherlands
+ *
+ *
+ * ------
+ *  Note
+ * ------
+ *
+ * I will assign copyright to PanEris (http://paneris.org) as soon as
+ * we have sorted out what sort of legal existence we need to have for
+ * that to make sense.  When WebMacro's "Simple Public License" is
+ * finalised, we'll offer it as an alternative license for Melati.
+ * In the meantime, if you want to use Melati on non-GPL terms,
+ * contact me!
+ */
+
 package org.melati.poem;
 
 import java.util.*;
 import java.sql.Date;
-import java.sql.Timestamp;
 import org.melati.util.*;
 
 public class Setting extends SettingBase {
   public Setting() {}
 
-  // programmer's domain-specific code here
+  private PoemType poemType = null;
+  private FieldAttributes valueAttributes = null;
+  private Object raw = null;
+  private Object cooked = null;
+
+  public Setting(Integer typefactory, String name, String value,
+		 String displayname, String description) {
+    this.typefactory = typefactory;
+    this.name = name;
+    this.value = value;
+    this.displayname = displayname;
+    this.description = description;
+  }
+
+  public PoemType getType() {
+    if (poemType == null) {
+      PoemTypeFactory f = getTypefactory();
+      if (f == null)
+	return StringPoemType.nullable;
+      poemType = getTypefactory().typeOf(getDatabase(), toTypeParameter());
+    }
+    return poemType;
+  }
+
+  public static class SettingValidationException extends PoemException {
+    public String name;
+
+    public SettingValidationException(String name, Exception problem) {
+      super(problem);
+      this.name = name;
+    }
+
+    public String getMessage() {
+      return "A problem arose updating the value of the `" + name +
+             "' setting:\n" + subException.getMessage();
+    }
+  }
+
+  public void setValue(String value) {
+    Object raw;
+    try {
+      raw = getType().rawOfString(value);
+    }
+    catch (Exception e) {
+      throw new SettingValidationException(name, e);
+    }
+
+    super.setValue(value);
+    this.raw = raw;
+    cooked = null;
+  }
+
+  public void setRaw(Object raw) {
+    String string;
+    try {
+      string = getType().stringOfRaw(raw);
+    }
+    catch (Exception e) {
+      throw new SettingValidationException(name, e);
+    }
+
+    super.setValue(string);
+    this.raw = raw;
+    cooked = null;
+  }
+
+  public Object getRaw() {
+    if (raw == null)
+      try {
+	raw = getType().rawOfString(getValue());
+      }
+      catch (Exception e) {
+	throw new SettingValidationException(name, e);
+      }
+
+    return raw;
+  }
+
+  public Object getCooked() {
+    if (cooked == null)
+      cooked = getType().cookedOfRaw(getRaw());
+    return cooked;
+  }
+
+  public static class SettingTypeMismatchException extends AppBugPoemException {
+    public String name;
+    public PoemTypeFactory type;
+    public String reqType;
+
+    public SettingTypeMismatchException(String name, PoemTypeFactory type,
+                                        String reqType) {
+      this.name = name;
+      this.type = type;
+      this.reqType = reqType;
+    }
+
+    public String getMessage() {
+      return "The setting `" + name + "' has type `" + type + "' but " +
+	     "the application asked for a value of type " + reqType;
+    }
+  }
+
+  public Integer getIntegerCooked() {
+    Object cooked = getCooked();
+    if (cooked == null)
+      return null;
+    else if (cooked instanceof Integer)
+      return (Integer)cooked;
+    else
+      throw new SettingTypeMismatchException(name, getTypefactory(), "Integer");
+  }
+
+  public String getStringCooked() {
+    Object cooked = getCooked();
+    if (cooked == null)
+      return null;
+    else if (cooked instanceof String)
+      return (String)cooked;
+    else
+      throw new SettingTypeMismatchException(name, getTypefactory(), "String");
+  }
+
+  private FieldAttributes valueAttributes() {
+    if (valueAttributes == null) {
+      Column c = getSettingTable().getValueColumn();
+      valueAttributes =
+	  new BaseFieldAttributes(
+              c.getName(), c.getDisplayName(), c.getDescription(), getType(),
+	      width == null ? 12 : width.intValue(),
+	      height == null ? 1 : height.intValue(),
+	      renderinfo);
+    }
+
+    return valueAttributes;
+  }
+
+  public Field getValueField() {
+    try {
+      return new Field(getRaw(), valueAttributes());
+    }
+    catch (AccessPoemException accessException) {
+      return new Field(accessException, valueAttributes());
+    }
+  }
 }
--- poem/src/main/java/org/melati/poem/SettingTable.java
+++ poem/src/main/java/org/melati/poem/SettingTable.java
@@ -2,10 +2,11 @@ package org.melati.poem;
 
 import java.util.*;
 import java.sql.Date;
-import java.sql.Timestamp;
 import org.melati.util.*;
 
 public class SettingTable extends SettingTableBase {
+
+  private static final Object nullEntry = new Object();
 
   public SettingTable(
       Database database, String name,
@@ -14,4 +15,93 @@ public class SettingTable extends SettingTableBase {
   }
 
   // programmer's domain-specific code here
+
+  private Hashtable cache = null;
+  private long cacheSerial = 0L;
+
+  public Object getCooked(String name) {
+    if (cache == null || cacheSerial != serial(PoemThread.transaction()))
+      cache = new Hashtable();
+    Object value = cache.get(name);
+    if (value == nullEntry)
+      return null;
+    else if (value != null)
+      return (String)value;
+    else {
+      Setting prop =
+          (Setting)getNameColumn().firstWhereEq(name);
+      if (prop == null) {
+	cache.put(name, nullEntry);
+	return null;
+      }
+      else {
+	Object propValue = prop.getCooked();
+	cache.put(name, propValue == null ? nullEntry : propValue);
+	return propValue;
+      }
+    }
+  }
+
+  public String get(String name) {
+    Object it = getCooked(name);
+    return it == null ? null : it.toString();
+  }
+
+  public static class UnsetException extends PoemException {
+    public String name;
+
+    public UnsetException(String name) {
+      this.name = name;
+    }
+
+    public String getMessage() {
+      return "The application's `" + name + "' parameter has not been set";
+      // FIXME include an URL
+    }
+  }
+
+  public Object getOrDie(String name) {
+    Object it = get(name);
+    if (it == null)
+      throw new UnsetException(name);
+    return it;
+  }
+
+  public Setting ensure(String name, PoemTypeFactory typefactory, Object value,
+			String displayname, String description) {
+    Setting setting = (Setting)getNameColumn().firstWhereEq(name);
+    if (setting != null)
+      return setting;
+    else {
+      setting = (Setting)newPersistent();
+      setting.setName(name);
+      setting.setDisplayname(displayname);
+      setting.setDescription(description);
+
+      setting.setUsereditable(true);
+
+      setting.setWidth(20);
+      setting.setHeight(1);
+
+      setting.setNullable(true);
+      setting.setSize(-1);
+      setting.setTypefactory(typefactory);
+
+      setting.setRaw(value);
+
+      return (Setting)getNameColumn().ensure(setting);
+    }
+  }
+
+  public Setting ensure(String name, String value,
+			String displayname, String description) {
+    return ensure(name, PoemTypeFactory.STRING, value,
+		  displayname, description);
+  }
+
+  public Setting ensure(String name, int value,
+			String displayname, String description) {
+    return ensure(name, PoemTypeFactory.INTEGER, new Integer(value),
+		  displayname, description);
+  }
 }

8f679342b87d76ef19d806030f53a4a76bf2620f
Chris Kimpton <chris@kimptoc.net>
2000-08-31 12:15:07 +0000

    implement some more of the poem odmg wrapper

--- melati-example-odmg/src/main/java/org/melati/poem/odmg/PoemTableAsDCollection.java
+++ melati-example-odmg/src/main/java/org/melati/poem/odmg/PoemTableAsDCollection.java
@@ -7,7 +7,7 @@ import org.melati.poem.*;
 
 class PoemTableAsDCollection implements org.odmg.DCollection
 {
-  public static final String cvs = "$Id";
+  public static final String cvs = "$Id$";
 
   private Table _wrappedTable = null;
 
@@ -25,19 +25,56 @@ class PoemTableAsDCollection implements org.odmg.DCollection
 	 return true;
   }
 
+  public boolean removeAll(Collection coll) 
+  {  
+    Iterator iter = coll.iterator();
+    while (iter.hasNext()) 
+    {
+       if (!remove(iter.next()))
+         return false;
+    }
+    return true;
+  }
 
+  /** removes the specified object from the collection 
+  * WARNING - this removes and commits it immediately!
+  */
+  public boolean remove(Object obj) 
+  {  
+    Persistent p = (Persistent)obj;
+    p.deleteAndCommit();
+    return true;
+  }
+
+  public Iterator iterator() 
+  { 
+    return new EnumerationIterator(_wrappedTable.selection());
+  }
+
+
+private class EnumerationIterator implements Iterator
+{
+  private Enumeration _enum = null;
+  EnumerationIterator(Enumeration enum)
+  {
+    _enum = enum;
+  }
+
+  public boolean hasNext() { return _enum.hasMoreElements(); }
+  public Object next() { return _enum.nextElement(); }
+  public void remove() { throw new org.odmg.NotImplementedException(); }
+}
+
+// the following have not been implemented - yet...
   public org.odmg.DCollection query(String queryString) { throw new org.odmg.NotImplementedException(); }
   public Object[] toArray(Object[] obj) { throw new org.odmg.NotImplementedException(); }
   public Object[] toArray() { throw new org.odmg.NotImplementedException(); }
   public boolean existsElement(String obj) { throw new org.odmg.NotImplementedException(); }
   public Object selectElement(String queryString) { throw new org.odmg.NotImplementedException(); }
-  public boolean remove(Object obj) { throw new org.odmg.NotImplementedException(); }
   public boolean contains(Object obj) { throw new org.odmg.NotImplementedException(); }
   public boolean addAll(Collection coll) { throw new org.odmg.NotImplementedException(); }
   public boolean containsAll(Collection coll) { throw new org.odmg.NotImplementedException(); }
   public boolean retainAll(Collection coll) { throw new org.odmg.NotImplementedException(); }
-  public boolean removeAll(Collection coll) { throw new org.odmg.NotImplementedException(); }
-  public Iterator iterator() { throw new org.odmg.NotImplementedException(); }
   public Iterator select(String queryString) { throw new org.odmg.NotImplementedException(); }
   public void clear() { throw new org.odmg.NotImplementedException(); }
 }

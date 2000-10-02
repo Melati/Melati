8d549b7ec0941442b04ae976d506d708c4e3e381
Tim Joyce <timj@paneris.org>
2000-10-02 11:18:01 +0000

    a counter for use in templates

---
+++ melati/src/main/java/org/melati/util/ContextUtil.java
@@ -0,0 +1,11 @@
+package org.melati.util;
+
+public class ContextUtil extends Object {
+
+    public ContextUtil() {
+    }
+
+    public Integer increment(Integer a) {
+        return new Integer(a.intValue() +1);
+    }
+}

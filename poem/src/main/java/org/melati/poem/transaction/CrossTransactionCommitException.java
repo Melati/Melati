8d43a1881452062ab2e6f74f930e57cb8d756513
William Chesters <williamc@paneris.org>
2000-11-13 20:33:38 +0000

    Make message more informative

--- poem/src/main/java/org/melati/poem/transaction/CrossTransactionCommitException.java
+++ poem/src/main/java/org/melati/poem/transaction/CrossTransactionCommitException.java
@@ -47,8 +47,16 @@
 package org.melati.util;
 
 public class CrossTransactionCommitException extends MelatiRuntimeException {
+
+  public Transactioned object;
+
+  public CrossTransactionCommitException(Transactioned object) {
+    this.object = object;
+  }
+
   public String getMessage() {
     return "An attempt was made to commit or roll back changes to an object " +
-           "from a different transaction than the one that made the changes";
+           object +
+           " from a different transaction than the one that made the changes";
   }
 }

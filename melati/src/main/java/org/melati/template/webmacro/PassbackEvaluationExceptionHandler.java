9c934db797739840030d88381988af70aea40d72
Tim Joyce <timj@paneris.org>
2001-04-17 14:26:48 +0000

    new Passback Exception Handler

---
+++ melati/src/main/java/org/melati/template/webmacro/PassbackEvaluationExceptionHandler.java
@@ -0,0 +1,111 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2000 Tim Joyce
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Tim Joyce <timj@paneris.org>
+ */
+
+package org.melati.template.webmacro;
+
+import java.io.IOException;
+
+import org.melati.template.MarkupLanguage;
+
+import org.webmacro.PropertyException;
+import org.webmacro.Context;
+import org.webmacro.engine.EvaluationExceptionHandler;
+import org.webmacro.engine.Variable;
+
+/**
+ * PassbackEvaluationExceptionHandler
+ *
+ * An implementation of EvaluationExceptionHandler which attempts to use a 
+ * templet to render the exception
+ *
+ */
+
+public class PassbackEvaluationExceptionHandler 
+  implements EvaluationExceptionHandler {
+
+   public void evaluate(Variable variable, 
+                        Context context, 
+                        Exception problem) 
+   throws PropertyException {
+     if (problem instanceof PropertyException)
+       throw (PropertyException) problem;
+     else 
+       throw new PropertyException("Error evaluating variable " 
+                                   + variable.getVariableName() + ": " 
+                                   + problem, problem);
+   }
+
+   public String expand(Variable variable, 
+                        Context context, 
+                        Exception problem) 
+   throws PropertyException {
+     MarkupLanguage ml = (MarkupLanguage)context.get("ml");
+     if (ml == null) throw new PropertyException(
+       "Error, to use the Passback Evaluation Exception Handler, you must " +
+       "place your MarkupLanguage in the context as $ml" + 
+       variable.getVariableName() + ": " + problem, problem);
+     Throwable underlying = problem;
+     if (problem instanceof PropertyException) {
+       PropertyException prob = (PropertyException)problem;
+       if (prob.caught != null) underlying = prob.caught;
+     }
+     try {
+       return ml.rendered(underlying);
+     } catch (IOException e) {
+       throw new PropertyException(
+         "The exception failed to render " + 
+         variable.getVariableName() + ": " + e, e);
+     }
+   }
+
+
+   public String warningString(String warningText) throws PropertyException {
+      throw new PropertyException("Evaluation warning: " + warningText);
+   }
+
+
+   public String errorString(String errorText) throws PropertyException {
+      throw new PropertyException("Evaluation error: " + errorText);
+   }
+}
+

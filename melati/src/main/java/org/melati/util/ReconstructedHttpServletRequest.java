d7248c7576fcf6fd608a4efd5029196f4c6d66a0
William Chesters <williamc@paneris.org>
2001-07-07 19:03:01 +0000

    Extend from new DelegatedHttpServletRequest which is transparent against 2.0/2.2 servlets; use session ID to reidentify Session objects rather than "=="

--- melati/src/main/java/org/melati/util/ReconstructedHttpServletRequest.java
+++ melati/src/main/java/org/melati/util/ReconstructedHttpServletRequest.java
@@ -51,76 +51,36 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.security.Principal;
 
-public class ReconstructedHttpServletRequest implements HttpServletRequest {
+public class ReconstructedHttpServletRequest
+    extends DelegatedHttpServletRequest {
 
   private HttpServletRequestParameters oldParams;
-  private HttpServletRequest newRequest;
 
   public ReconstructedHttpServletRequest(
       HttpServletRequestParameters oldParams, HttpServletRequest newRequest)
           throws ReconstructedHttpServletRequestMismatchException {
+
+    super(newRequest);
+    this.oldParams = oldParams;
+
+    HttpSession session = newRequest.getSession(false);
     if (!oldParams.requestURL.equals(
             HttpUtils.getRequestURL(newRequest).toString()) ||
         !(oldParams.queryString == null ?
             newRequest.getQueryString() == null :
             oldParams.queryString.equals(newRequest.getQueryString())) ||
-        oldParams.session != newRequest.getSession(false))
+        !(session != null && session.getId() != null &&
+          session.getId().equals(oldParams.sessionID)))
       throw new ReconstructedHttpServletRequestMismatchException(oldParams,
                                                                  newRequest);
-
-    this.oldParams = oldParams;
-    this.newRequest = newRequest;
   }
 
   public void setNewRequest(HttpServletRequest newRequest) {
-    this.newRequest = newRequest;
+    peer = newRequest;
   }
 
   public HttpServletRequest getNewRequest() {
-    return newRequest;
-  }
-
-  public int getContentLength() {
-    return newRequest.getContentLength();
-  }
-
-  public String getContentType() {
-    return newRequest.getContentType();
-  }
-
-  public String getProtocol() {
-    return newRequest.getProtocol();
-  }
-
-  public String getScheme() {
-    return newRequest.getScheme();
-  }
-
-  public String getServerName() {
-    return newRequest.getServerName();
-  }
-
-  public int getServerPort() {
-    return newRequest.getServerPort();
-  }
-
-  public String getRemoteAddr() {
-    return newRequest.getRemoteAddr();
-  }
-
-  public String getRemoteHost() {
-    return newRequest.getRemoteHost();
-  }
-
-  /**
-   * @deprecated in Servlet API 2.2
-   */
-  public String getRealPath(String path) {
-    return newRequest.getRealPath(path);
-  }
-
-  public ServletInputStream getInputStream() throws IOException {
-    return newRequest.getInputStream();
+    return peer;
   }
 
   /**
@@ -148,149 +108,11 @@ public class ReconstructedHttpServletRequest implements HttpServletRequest {
     return oldParams.parameters.keys();
   }
 
-  public Object getAttribute(String name) {
-    return newRequest.getAttribute(name);
-  }
-
-  public BufferedReader getReader() throws IOException {
-    return newRequest.getReader();
-  }
-
-  public String getCharacterEncoding() {
-    return newRequest.getCharacterEncoding();
-  }
-
-  public Cookie[] getCookies() {
-    return newRequest.getCookies();
-  }
-
   /**
    * From the <B>old</B> request.
    */
 
   public String getMethod() {
     return oldParams.method;
-  }
-
-  public String getRequestURI() {
-    return newRequest.getRequestURI();
-  }
-
-  public String getServletPath() {
-    return newRequest.getServletPath();
-  }
-
-  public String getPathInfo() {
-    return newRequest.getPathInfo();
-  }
-
-  public String getPathTranslated() {
-    return newRequest.getPathTranslated();
-  }
-
-  public String getQueryString() {
-    return newRequest.getQueryString();
-  }
-
-  public String getRemoteUser() {
-    return newRequest.getRemoteUser();
-  }
-
-  public String getAuthType() {
-    return newRequest.getAuthType();
-  }
-
-  public String getHeader(String name) {
-    return newRequest.getHeader(name);
-  }
-
-  public int getIntHeader(String name) {
-    return newRequest.getIntHeader(name);
-  }
-
-  public long getDateHeader(String name) {
-    return newRequest.getDateHeader(name);
-  }
-
-  public Enumeration getHeaderNames() {
-    return newRequest.getHeaderNames();
-  }
-
-  public HttpSession getSession(boolean create) {
-    return newRequest.getSession(create);
-  }
-
-  public String getRequestedSessionId() {
-    return newRequest.getRequestedSessionId();
-  }
-
-  public boolean isRequestedSessionIdValid() {
-    return newRequest.isRequestedSessionIdValid();
-  }
-
-  public boolean isRequestedSessionIdFromCookie() {
-    return newRequest.isRequestedSessionIdFromCookie();
-  }
-
-  /**
-   * @deprecated in Servlet API 2.2
-   */
-  public boolean isRequestedSessionIdFromUrl() {
-    return newRequest.isRequestedSessionIdFromUrl();
-  }
-
-  // added for Servlet API 2.2
-  public String getContextPath() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public Enumeration getHeaders(String arg) {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public HttpSession getSession() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public Principal getUserPrincipal() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public boolean isRequestedSessionIdFromURL() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public boolean isUserInRole(String arg) {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public Enumeration getAttributeNames() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public Locale getLocale()  {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public Enumeration getLocales() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-    /*
-  public RequestDispatcher getRequestDispatcher(String arg) {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-    */
-
-  public boolean isSecure() {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public void removeAttribute(String arg) {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
-  }
-
-  public void setAttribute(String arg1, Object arg2) {
-    throw new RuntimeException("Unsupported call to Servlet 2.2 API");
   }
 }

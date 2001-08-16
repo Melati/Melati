c7c49ee6d724358fb269e61dcae1890cb5f6f2e9
Tim Joyce <timj@paneris.org>
2001-08-16 17:14:33 +0000

    new MD5 util for encoding

---
+++ melati/src/main/java/org/melati/util/MD5Util.java
@@ -0,0 +1,84 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2000 Tim Joyce
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Tim Joyce <timj@paneris.org>
+ *     http://paneris.org/
+ *     68 Sandbanks Rd, Poole, Dorset. BH14 8BY. UK
+ */
+
+package org.melati.util;
+
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+
+/**
+ * MD5 - utils for encoding using MD5
+ */
+
+public class MD5Util {
+
+  public static String encode(String in) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      byte[] digest = md.digest(in.getBytes());
+      // and URLEncode so that this can be used in cookies etc
+      return URLEncoder.encode(new String(digest));
+    } catch (Exception e) {
+      throw new MelatiBugMelatiException("For some reason I couldn't encode the password!", e);
+    }
+  }
+
+  
+   /**
+     * Test harness
+     */
+   public static void main(String arg[]) {
+
+      String in = "FIXME";
+      if (arg.length > 0) {
+        in = arg[1];
+      }
+      System.out.println("arg:" + in + ":");
+      System.out.println(":" + encode(in) + ":");
+      System.out.println(":" + encode(in) + ":");
+
+   }
+
+}

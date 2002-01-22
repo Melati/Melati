488f711e12a9f70fa968e20817f70586f25144c1
Tim Pizey <timp@paneris.org>
2002-01-22 19:13:01 +0000

    Add getMessage()

--- poem/src/main/java/org/melati/poem/NoSuchTablePoemException.java
+++ poem/src/main/java/org/melati/poem/NoSuchTablePoemException.java
@@ -48,8 +48,14 @@ package org.melati.poem;
 public class NoSuchTablePoemException extends NormalPoemException {
   public Database database;
   public String name;
+
   public NoSuchTablePoemException(Database database, String name) {
     this.database = database;
     this.name = name;
   }
+
+  public String getMessage() {
+    return "Table " + name + " not found in " + database + ".";
+  }
+
 }

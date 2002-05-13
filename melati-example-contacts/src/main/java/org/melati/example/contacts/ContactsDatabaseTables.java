8f7f09c5e9a8dd4548100af6d34ae905b46e9441
Tim Pizey <timp@paneris.org>
2002-05-13 18:22:52 +0000

    Contacts haven't been regenerated since before these two were generated by DSD

---
+++ melati-example-contacts/src/main/java/org/melati/example/contacts/ContactsDatabaseTables.java
@@ -0,0 +1,11 @@
+package org.melati.doc.example.contacts;
+
+import org.melati.doc.example.contacts.generated.*;
+import java.util.*;
+import java.sql.Date;
+import java.sql.Timestamp;
+import org.melati.poem.*;
+
+public interface ContactsDatabaseTables extends ContactsDatabaseTablesBase {
+  // programmer's domain-specific code here
+}

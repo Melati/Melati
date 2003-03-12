573e1918656175ac49f8a57ccac25e41fa081f60
Tim Joyce <timj@paneris.org>
2003-03-12 20:32:25 +0000

    in the event of an error in a multipart post, it is useful to have a template context so that errors are reported nicely

--- melati/src/main/java/org/melati/servlet/TemplateServlet.java
+++ melati/src/main/java/org/melati/servlet/TemplateServlet.java
@@ -107,6 +107,8 @@ public abstract class TemplateServlet extends PoemServlet {
     TemplateContext templateContext =
                         templateEngine.getTemplateContext(melati);
 
+    melati.setTemplateContext(templateContext);
+    
     // If we have an multipart form, we use a different template context
     // which allows us to access the uploaded files as well as fields.
     String contentType = melati.getRequest().getHeader("content-type");

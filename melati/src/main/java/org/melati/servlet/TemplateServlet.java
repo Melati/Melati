9d8b24eec5bb7041c00b59ec584fdc6f4240ae27
Tim Pizey <timp@paneris.org>
2004-11-30 22:45:24 +0000

    Initialise the engine with a servlet, hence enableing servlet container classpath (including lib jars) to be used.

--- melati/src/main/java/org/melati/servlet/TemplateServlet.java
+++ melati/src/main/java/org/melati/servlet/TemplateServlet.java
@@ -85,7 +85,7 @@ public abstract class TemplateServlet extends PoemServlet {
     try {
       templateEngine = melatiConfig.getTemplateEngine();
       if (templateEngine != null)
-        templateEngine.init(melatiConfig);
+        templateEngine.init(melatiConfig, this);
     } catch (TemplateEngineException e) {
       // log it to system.err as ServletExceptions go to the
       // servlet runner log (eg jserv.log), and don't have a stack trace!

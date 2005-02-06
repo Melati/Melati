f4ff6dd59679c7b4f0c382dd8d10192e1e10d682
Tim Pizey <timp@paneris.org>
2005-02-06 20:50:58 +0000

    checkstyle: made final

--- melati-example-odmg/src/main/java/org/melati/poem/odmg/Transaction.java
+++ melati-example-odmg/src/main/java/org/melati/poem/odmg/Transaction.java
@@ -47,12 +47,12 @@ package org.melati.poem.odmg;
  * An ODMG Transaction.
  */
 
-class Transaction implements org.odmg.Transaction {
-  public static final String cvs = "$Id";
+final class Transaction implements org.odmg.Transaction {
+  public static String cvs = "$Id";
 
   private Database _db = null;
 
-  static final Transaction getNewTransaction(org.odmg.Database db) { 
+  static Transaction getNewTransaction(org.odmg.Database db) { 
     return new Transaction(db); 
   }
   private Transaction(org.odmg.Database db) {
@@ -89,21 +89,27 @@ class Transaction implements org.odmg.Transaction {
   public void join() {
     throw new org.odmg.NotImplementedException(); 
   }
+
   public void abort() {
     throw new org.odmg.NotImplementedException(); 
   }
+
   public void checkpoint() {
     throw new org.odmg.NotImplementedException(); 
   }
+
   public void leave() {
     throw new org.odmg.NotImplementedException(); 
   }
+
   public void lock(Object obj, int code) { 
     throw new org.odmg.NotImplementedException(); 
   }
+
   public boolean tryLock(Object obj, int code) {
     throw new org.odmg.NotImplementedException(); 
   }
+
   public boolean isOpen() {
     throw new org.odmg.NotImplementedException(); 
   }

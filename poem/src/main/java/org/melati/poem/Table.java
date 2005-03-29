a4f82289b78b9090e140bccd34dc799e0f4918aa
Tim Pizey <timp@paneris.org>
2005-03-29 15:22:18 +0000

    Check if the field iname s longer than max before chopping it

--- poem/src/main/java/org/melati/poem/Table.java
+++ poem/src/main/java/org/melati/poem/Table.java
@@ -2505,7 +2505,8 @@ public class Table implements Selectable {
             // Don't want to take account of non-melati indices
             String expectedIndex = getName().toUpperCase() + "_" + 
                                    columnName.toUpperCase() + "_INDEX";
-            expectedIndex = expectedIndex.substring(0,dbms().maxFieldLength());
+            if (expectedIndex.length() > dbms().maxFieldLength()) 
+              expectedIndex = expectedIndex.substring(0,dbms().maxFieldLength());
             if (mdIndexName.toUpperCase().equals(expectedIndex)) {
               column.unifyWithIndex(index);
               dbHasIndexForColumn.put(column, Boolean.TRUE);

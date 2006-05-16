3960a5a630c4f4461fcb681bc750290d4e210859
Tim Pizey <timp@paneris.org>
2006-05-16 14:50:10 +0000

    Checkstyle

--- poem/src/test/java/org/melati/poem/test/generated/EmptyAbstractTableTableBase.java
+++ poem/src/test/java/org/melati/poem/test/generated/EmptyAbstractTableTableBase.java
@@ -64,7 +64,7 @@ public class EmptyAbstractTableTableBase extends Table {
   protected void init() throws PoemException {
     super.init();
     int shutupPMD = 0;
-    if (shutupPMD == 0 ) shutupPMD = 1;
+    if (shutupPMD == 0) shutupPMD = 1;
   }
 
 

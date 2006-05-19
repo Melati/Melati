353c0f68eb5a52530ee0c64947be1f9a9ac3fe91
Tim Pizey <timp@paneris.org>
2006-05-19 21:30:27 +0000

    Rename Ml to MarkupLanguage

--- melati/src/main/java/org/melati/Melati.java
+++ melati/src/main/java/org/melati/Melati.java
@@ -575,7 +575,7 @@ public class Melati {
    * 
    * @param ml The ml to set.
    */
-  public void setMl(MarkupLanguage ml) {
+  public void setMarkupLanguage(MarkupLanguage ml) {
     this.ml = ml;
   }
   
@@ -833,7 +833,7 @@ public class Melati {
    * @return - PassbackVariableExceptionHandler specific to the
    * template engine
    *
-   * @see org.melati.template.MarkupLanguage#rendered
+   * @see org.melati.template.MarkupLanguage#rendered(Object)
    * @see org.melati.poem.TailoredQuery
    */
   public Object getPassbackVariableExceptionHandler() {

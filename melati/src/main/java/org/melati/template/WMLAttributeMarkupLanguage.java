0a61d5d33caa54a3ba8065b7e07823c2989be337
Tim Pizey <timp@paneris.org>
2006-05-19 20:48:22 +0000

    Renamed from AttributeWMLMarkupLanguage

---
+++ melati/src/main/java/org/melati/template/WMLAttributeMarkupLanguage.java
@@ -0,0 +1,81 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2000 William Chesters
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     William Chesters <williamc@paneris.org>
+ *     http://paneris.org/~williamc
+ *     Obrechtstraat 114, 2517VX Den Haag, The Netherlands
+ */
+
+package org.melati.template;
+
+import java.io.IOException;
+import org.melati.poem.AccessPoemException;
+
+/**
+ * A WML based markup language suitable for markup within an attribute tag.
+ * @todo Delete this, it should be identical to AttributeHTMLMarkupLanguage, 
+ * WML has same entities as HTML.
+ * 
+ */
+public class WMLAttributeMarkupLanguage extends WMLMarkupLanguage
+    implements MarkupLanguage, AttributeMarkupLanguage {
+
+
+  public WMLAttributeMarkupLanguage(WMLMarkupLanguage wml) {
+    super("wml_attr", wml);
+  }
+
+  /**
+   * AccessPoemException is handled differently in an Attribute 
+   * than it is in the main body of a page.
+   */
+  public String rendered(AccessPoemException e) throws IOException {
+    try {
+      melati.getWriter().write("[Access denied to ");
+      render(e.token);
+      melati.getWriter().write("]");
+    }
+    catch (Exception g) {
+      melati.getWriter().write("[UNRENDERABLE EXCEPTION!]");
+    }
+
+    return "";
+  }
+}

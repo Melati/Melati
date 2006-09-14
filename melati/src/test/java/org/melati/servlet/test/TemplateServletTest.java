4bbe192fbfdf1683b2a7ecc8f1af10f90d6aa642
Tim Pizey <timp@paneris.org>
2006-09-14 12:35:42 +0000

    Comment out import as its use is commented out

--- melati/src/test/java/org/melati/servlet/test/TemplateServletTest.java
+++ melati/src/test/java/org/melati/servlet/test/TemplateServletTest.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.melati.util.HttpServletRequestParameters;
+//import org.melati.util.HttpServletRequestParameters;
 
 import com.mockobjects.dynamic.Mock;
 import com.mockobjects.dynamic.OrderedMock;

21ffebb37038863aea06e3ea20ceab7f9057cb0b
Tim Pizey <timp@paneris.org>
2006-10-05 15:04:40 +0000

    Add table creation test

--- poem/src/test/java/org/melati/poem/test/PoemDatabaseTest.java
+++ poem/src/test/java/org/melati/poem/test/PoemDatabaseTest.java
@@ -5,9 +5,14 @@ import java.sql.SQLException;
 import java.util.Enumeration;
 
 import org.melati.poem.Capability;
+import org.melati.poem.ColumnInfo;
+import org.melati.poem.PoemDatabase;
+import org.melati.poem.PoemThread;
+import org.melati.poem.TableInfo;
 import org.melati.poem.User;
 import org.melati.poem.UserTable;
 import org.melati.poem.dbms.Dbms;
+import org.melati.poem.Database;
 
 /**
  * Test the features of all Poem databases.
@@ -125,7 +130,6 @@ public class PoemDatabaseTest extends PoemTestCase {
   public final void testAdministratorUser() {
     User u = getDb().getUserTable().administratorUser();
     assertEquals(u.getPassword(), "FIXME");
-    // TODO Implement administratorUser().
   }
 
   /**
@@ -174,5 +178,325 @@ public class PoemDatabaseTest extends PoemTestCase {
     getDb().setLogSQL(true);
     assertTrue(getDb().logSQL());
   }
+  /**
+   * @see Database#connect(String, String, String, String, int)
+   */
+  public void testConnect() {
+    
+    PoemDatabase db = new PoemDatabase();
+    db.connect("org.melati.poem.dbms.Hsqldb",
+                            "jdbc:hsqldb:/dist/melati/db/m2", "sa", "", 3); 
+    assertTrue(db.getClass().getName() == "org.melati.poem.PoemDatabase"); 
+    db.shutdown();
+    db.disconnect();
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.disconnect()'
+   */
+  public void testDisconnect() {
+    // see above
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.shutdown()'
+   */
+  public void testShutdown() {
+    // see above
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.addTableAndCommit(TableInfo, String)'
+   */
+  public void testAddTableAndCommit() {
+    TableInfo info = (TableInfo)getDb().getTableInfoTable().newPersistent();
+    info.setName("junit");
+    info.setDisplayname("Junit created table");
+    info.setDisplayorder(13);
+    info.setSeqcached(new Boolean(false));
+    info.setCategory_unsafe(new Integer(1));
+    info.setCachelimit(0);
+    info.makePersistent();
+    getDb().addTableAndCommit(info, "id");
+    Enumeration cols = getDb().getColumnInfoTable().
+                           getTableinfoColumn().selectionWhereEq(info.troid());
+     while (cols.hasMoreElements()) {
+       ColumnInfo c = (ColumnInfo)cols.nextElement();
+       c.delete();
+     }
+    info.delete();
+    getDb().sqlUpdate("DROP TABLE JUNIT");
+    PoemThread.commit();
+    getDb().shutdown();
+    getDb().disconnect();
+
+  }
+
+  /**
+   * @see org.melati.poem.Database.addConstraints()
+   */
+  public void testAddConstraints() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.setTransactionsMax(int)'
+   */
+  public void testSetTransactionsMax() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getTransactionsCount()'
+   */
+  public void testGetTransactionsCount() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getFreeTransactionsCount()'
+   */
+  public void testGetFreeTransactionsCount() {
+
+  }
+
+  /*
+   * @see org.melati.poem.Database.poemTransaction(int)
+   */
+  public void testPoemTransaction() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.transaction(int)'
+   */
+  public void testTransaction() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.isFree(PoemTransaction)'
+   */
+  public void testIsFree() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.beginExclusiveLock()'
+   */
+  public void testBeginExclusiveLock() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.endExclusiveLock()'
+   */
+  public void testEndExclusiveLock() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.inSession(AccessToken, PoemTask)'
+   */
+  public void testInSession() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.beginSession(AccessToken)'
+   */
+  public void testBeginSession() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.endSession()'
+   */
+  public void testEndSession() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.inCommittedTransaction(AccessToken, PoemTask)'
+   */
+  public void testInCommittedTransaction() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getTable(String)'
+   */
+  public void testGetTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.tables()'
+   */
+  public void testTables() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.columns()'
+   */
+  public void testColumns() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getTableInfoTable()'
+   */
+  public void testGetTableInfoTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getTableCategoryTable()'
+   */
+  public void testGetTableCategoryTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getColumnInfoTable()'
+   */
+  public void testGetColumnInfoTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getCapabilityTable()'
+   */
+  public void testGetCapabilityTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getGroupTable()'
+   */
+  public void testGetGroupTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getGroupMembershipTable()'
+   */
+  public void testGetGroupMembershipTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getGroupCapabilityTable()'
+   */
+  public void testGetGroupCapabilityTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getSettingTable()'
+   */
+  public void testGetSettingTable() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.sqlUpdate(String)'
+   */
+  public void testSqlUpdate() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.givesCapabilitySQL(User, Capability)'
+   */
+  public void testGivesCapabilitySQL() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.guestAccessToken()'
+   */
+  public void testGuestAccessToken() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.setCanAdminister()'
+   */
+  public void testSetCanAdminister() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.setCanAdminister(String)'
+   */
+  public void testSetCanAdministerString() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.trimCache(int)'
+   */
+  public void testTrimCache() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.uncacheContents()'
+   */
+  public void testUncacheContents() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.referencesTo(Persistent)'
+   */
+  public void testReferencesToPersistent() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.dumpCacheAnalysis()'
+   */
+  public void testDumpCacheAnalysis() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.dump()'
+   */
+  public void testDump() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.quotedName(String)'
+   */
+  public void testQuotedName() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.getCommittedConnection()'
+   */
+  public void testGetCommittedConnection() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.logCommits()'
+   */
+  public void testLogCommits() {
+
+  }
+
+  /*
+   * Test method for 'org.melati.poem.Database.setLogCommits(boolean)'
+   */
+  public void testSetLogCommits() {
+
+  }
+
 
 }

40899c10f171600c12c51089786202111eab1d7b
Tim Pizey <timp@paneris.org>
2006-10-25 15:19:11 +0000

    Change ancestory of exception so that it renders nicely in the admin system

--- melati-example-contacts/src/main/java/org/melati/example/contacts/Contact.java
+++ melati-example-contacts/src/main/java/org/melati/example/contacts/Contact.java
@@ -8,36 +8,36 @@ import org.melati.poem.AccessPoemException;
 import org.melati.poem.User;
 import org.melati.poem.PoemThread;
 import org.melati.util.EnumUtils;
-import org.melati.util.MelatiRuntimeException;
+import org.melati.admin.AnticipatedException;
 import org.melati.util.Treeable;
 /**
- * A <code>Contact</code> object, embellished from the original, 
+ * A <code>Contact</code> object, embellished from the original,
  * Melati POEM generated, programmer modifiable stub.
- * 
- * <p> 
- * Description: 
- *   A Contact. 
+ *
+ * <p>
+ * Description:
+ *   A Contact.
  * </p>
- * 
- * <table> 
+ *
+ * <table>
  * <tr><th colspan='3'>
  * Field summary for SQL table <code>Contact</code>
  * </th></tr>
  * <tr><th>Name</th><th>Type</th><th>Description</th></tr>
- * <tr><td> id </td><td> Integer </td><td> &nbsp; </td></tr> 
- * <tr><td> name </td><td> String </td><td> Contact Name </td></tr> 
- * <tr><td> owner </td><td> Contact </td><td> Contact who owns this contact 
- * </td></tr> 
- * <tr><td> address </td><td> String </td><td> Contact Address </td></tr> 
- * <tr><td> updates </td><td> Integer </td><td> How many times has this 
- * record been updated? </td></tr> 
- * <tr><td> lastupdated </td><td> Date </td><td> When was this last updated? 
- * </td></tr> 
- * <tr><td> lastupdateuser </td><td> User </td><td> Who last updated this? 
- * </td></tr> 
- * </table> 
- * 
- * @generator org.melati.poem.prepro.TableDef#generateMainJava 
+ * <tr><td> id </td><td> Integer </td><td> &nbsp; </td></tr>
+ * <tr><td> name </td><td> String </td><td> Contact Name </td></tr>
+ * <tr><td> owner </td><td> Contact </td><td> Contact who owns this contact
+ * </td></tr>
+ * <tr><td> address </td><td> String </td><td> Contact Address </td></tr>
+ * <tr><td> updates </td><td> Integer </td><td> How many times has this
+ * record been updated? </td></tr>
+ * <tr><td> lastupdated </td><td> Date </td><td> When was this last updated?
+ * </td></tr>
+ * <tr><td> lastupdateuser </td><td> User </td><td> Who last updated this?
+ * </td></tr>
+ * </table>
+ *
+ * @generator org.melati.poem.prepro.TableDef#generateMainJava
  */
 public class Contact extends ContactBase implements Treeable {
   /**
@@ -45,32 +45,32 @@ public class Contact extends ContactBase implements Treeable {
    * @author timp
    * @param message
    */
-  public class DescendantParentException extends MelatiRuntimeException {
+  public class DescendantParentException extends AnticipatedException {
     private static final long serialVersionUID = 1L;
     public DescendantParentException(String message) {
       super(message);
     }
   }
 /**
-  * Constructor 
+  * Constructor
   * for a <code>Persistent</code> <code>Contact</code> object.
   * <p>
-  * Description: 
-  *   A Contact. 
+  * Description:
+  *   A Contact.
   * </p>
-  * 
-  * @generator org.melati.poem.prepro.TableDef#generateMainJava 
+  *
+  * @generator org.melati.poem.prepro.TableDef#generateMainJava
   */
   public Contact() { }
   // programmer's domain-specific code here
-  
+
   public boolean isIn(Category category) {
      ContactsDatabase db = (ContactsDatabase)getContactsDatabaseTables();
      String sql = db.quotedName("contact") + " = " + getTroid() + " AND " +
        db.quotedName("category") + " = " + category.getTroid();
      return db.getContactCategoryTable().exists(sql);
   }
-  
+
   protected void writeLock() {
     super.writeLock();
     setLastupdated_unsafe(new java.sql.Date(new java.util.Date().getTime()));
@@ -78,17 +78,17 @@ public class Contact extends ContactBase implements Treeable {
       setLastupdateuser_unsafe(((User)PoemThread.accessToken()).getTroid());
     else
       setLastupdateuser_unsafe(new Integer(1));
-      
+
     Integer count = getUpdates();
     if (count == null) count = new Integer(0);
     setUpdates_unsafe(new Integer(count.intValue()+1));
   }
-  
+
   public Treeable[] getChildren() {
     return (Contact.arrayOf(getContactTable().getOwnerColumn().
                selectionWhereEq(troid())));
   }
-  
+
   public ArrayList getAncestors() {
     ArrayList l = new ArrayList();
     Contact p = getOwner();
@@ -105,7 +105,7 @@ public class Contact extends ContactBase implements Treeable {
     }
     super.setOwner(cooked);
   }
-  
+
   public static Treeable[] arrayOf(Vector v) {
     Treeable[] arr;
     synchronized (v) {
@@ -118,6 +118,6 @@ public class Contact extends ContactBase implements Treeable {
     Vector v = EnumUtils.vectorOf(e);
     return arrayOf(v);
   }
-  
+
 }
 

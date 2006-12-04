5451aa8ccbdd21e12e58ba6353519bc2931d9d3a
Tim Pizey <timp@paneris.org>
2006-12-04 22:49:15 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/servlet/test/ExceptionTemplateServlet.java
@@ -0,0 +1,28 @@
+package org.melati.servlet.test;
+
+import org.melati.Melati;
+import org.melati.servlet.TemplateServlet;
+import org.melati.template.ServletTemplateContext;
+
+/**
+ * @author timp
+ * @since 5/12/2006
+ *
+ */
+public class ExceptionTemplateServlet extends TemplateServlet {
+
+  /**
+   * Shut eclipse up.
+   */
+  private static final long serialVersionUID = 1L;
+
+  public ExceptionTemplateServlet() {
+    super();
+  }
+
+  protected String doTemplateRequest(Melati melati,
+      ServletTemplateContext templateContext) throws Exception {
+    throw new Exception("A problem");
+  }
+
+}

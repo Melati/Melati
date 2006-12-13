e2fcc77d6c00ebb46046d5d3124ed87584a16e10
Tim Pizey <timp@paneris.org>
2006-12-13 16:11:52 +0000

    Javadoc

--- poem/src/main/java/org/melati/poem/transaction/Transactioned.java
+++ poem/src/main/java/org/melati/poem/transaction/Transactioned.java
@@ -152,7 +152,7 @@ public abstract class Transactioned {
    * write locks on the object before claiming the next write
    * lock. Then it will block until none have read locks.
    * <p>
-   * Finally it calls ensureValid(Transaction).
+   * Finally it calls {@link #ensureValid(Transaction)}.
    */
   protected void writeLock(Transaction transaction) {
 

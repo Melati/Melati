70d30608aa6593970c3b12cd8cadaa010934f9ab
Tim Pizey <timp@paneris.org>
2007-02-09 15:12:14 +0000

    Copied from microsoft site

---
+++ poem/src/test/java/org/melati/poem/dbms/test/DebugMSAccess.java
@@ -0,0 +1,14 @@
+package org.melati.poem.dbms.test;
+
+public class DebugMSAccess {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+   // String query = "SELECT \"id\" + 1 FROM \"user\" AS t1 WHERE " + 
+   // "NOT EXISTS (SELECT * FROM \"user\" AS t2 WHERE t2.\"id\" = t1.\"id\" + 1)  LIMIT 1"; 
+  }
+
+}

807c2c9e49118e4eb9a66ca0a744ad555f08a50c
Tim Pizey <timp@paneris.org>
2007-02-10 10:28:06 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/template/test/TreeTestCase.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.melati.template.test;
+
+import org.melati.poem.test.PoemTestCase;
+
+/**
+ * @author timp
+ * @since 10 Feb 2007
+ *
+ */
+public class TreeTestCase extends PoemTestCase {
+  /** Default db name */
+  public static final String databaseName = "tree";
+
+  /**
+   * 
+   */
+  public TreeTestCase() {
+  }
+
+  /**
+   * @param name
+   */
+  public TreeTestCase(String name) {
+    super(name);
+  }
+  protected void databaseUnchanged() { 
+    assertEquals("Setting changed", 0, getDb().getSettingTable().count());
+    assertEquals("Group changed", 1, getDb().getGroupTable().count());
+    assertEquals("GroupMembership changed", 1, getDb().getGroupMembershipTable().count());
+    assertEquals("Capability changed", 5, getDb().getCapabilityTable().count());
+    assertEquals("GroupCapability changed", 1, getDb().getGroupCapabilityTable().count());
+    assertEquals("TableCategory changed", 3, getDb().getTableCategoryTable().count());
+    assertEquals("User changed", 2, getDb().getUserTable().count());
+    //ColumnInfo newOne = null; 
+    //try{ 
+    //  newOne = (ColumnInfo)getDb().getColumnInfoTable().getObject(69);
+    //} catch (Exception e) {}
+    //if (newOne != null) { 
+    //  System.err.println(newOne.getName() + " " + newOne.getTableinfo().getName());
+    //}
+    assertEquals("ColumnInfo changed", 72, getDb().getColumnInfoTable().count());
+    assertEquals("TableInfo changed", 10, getDb().getTableInfoTable().count());
+    checkTablesAndColumns(10,72);
+  }
+
+}

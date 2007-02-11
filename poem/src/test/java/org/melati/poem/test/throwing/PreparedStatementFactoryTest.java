1be64c329f7c239964e012e7f48e1c96db5b276e
Tim Pizey <timp@paneris.org>
2007-02-11 22:44:57 +0000

    Remove db so that it is reinitialised and use different throwers

--- poem/src/test/java/org/melati/poem/test/throwing/PreparedStatementFactoryTest.java
+++ poem/src/test/java/org/melati/poem/test/throwing/PreparedStatementFactoryTest.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 import org.melati.poem.Database;
 import org.melati.poem.PoemDatabaseFactory;
 import org.melati.poem.PreparedSQLSeriousPoemException;
+import org.melati.poem.PreparedStatementFactory;
 import org.melati.poem.SQLPoemException;
 import org.melati.poem.dbms.test.ThrowingConnection;
 import org.melati.poem.dbms.test.ThrowingPreparedStatement;
@@ -26,18 +27,26 @@ public class PreparedStatementFactoryTest extends
   public PreparedStatementFactoryTest(String name) {
     super(name);
   }
+  protected void setUp() throws Exception {
+    PoemDatabaseFactory.removeDatabase(databaseName);
+    super.setUp();
+    assertEquals("org.melati.poem.dbms.test.HsqldbThrower",getDb().getDbms().getClass().getName());
+  }
+
 
   public Database getDatabase(String name) {
     Properties defs = databaseDefs();
     String pref = "org.melati.poem.test.PoemTestCase." + name + ".";
     maxTrans = new Integer(getOrDie(defs, pref + "maxtransactions")).intValue();
-    return PoemDatabaseFactory.getDatabase(name, getOrDie(defs, pref + "url"),
-        getOrDie(defs, pref + "user"), getOrDie(defs, pref + "password"),
+    return PoemDatabaseFactory.getDatabase(name, 
+        getOrDie(defs, pref + "url"),
+        getOrDie(defs, pref + "user"), 
+        getOrDie(defs, pref + "password"),
         getOrDie(defs, pref + "class"),
-        "org.melati.poem.dbms.test.HsqldbThrower", new Boolean(getOrDie(defs,
-            pref + "addconstraints")).booleanValue(), new Boolean(getOrDie(
-            defs, pref + "logsql")).booleanValue(), new Boolean(getOrDie(defs,
-            pref + "logcommits")).booleanValue(), maxTrans);
+        "org.melati.poem.dbms.test.HsqldbThrower", 
+        new Boolean(getOrDie(defs, pref + "addconstraints")).booleanValue(), 
+        new Boolean(getOrDie(defs, pref + "logsql")).booleanValue(), 
+        new Boolean(getOrDie(defs, pref + "logcommits")).booleanValue(), maxTrans);
   }
 
   public void testGet() {
@@ -48,6 +57,7 @@ public class PreparedStatementFactoryTest extends
     ThrowingConnection.startThrowing("prepareStatement");
     try {
       super.testPreparedStatement();
+      fail("Should have bombed");
     } catch (SQLPoemException e) {
       assertEquals("Connection bombed", e.innermostException().getMessage());
     }
@@ -63,9 +73,12 @@ public class PreparedStatementFactoryTest extends
   }
 
   public void testResultSet() throws Exception {
-    ThrowingPreparedStatement.startThrowing("executeQuery");
+    PreparedStatementFactory it = new PreparedStatementFactory(getDb(),
+        getDb().getUserTable().selectionSQL(null,null,null,true,false));
     try {
-      super.testResultSet();
+      ThrowingPreparedStatement.startThrowing("executeQuery");
+      it.resultSet();
+      fail("Should have bombed");
     } catch (PreparedSQLSeriousPoemException e) {
       assertEquals("PreparedStatement bombed", e.innermostException().getMessage());
     }

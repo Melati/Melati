b73853ff1043540e0f033a8df6b35f77d9f09dd8
Tim Pizey <timp@paneris.org>
2007-02-12 12:09:39 +0000

    Initial version

---
+++ poem/src/test/java/org/melati/poem/test/throwing/CachedSelectionTest.java
@@ -0,0 +1,64 @@
+package org.melati.poem.test.throwing;
+
+import org.melati.poem.Database;
+import org.melati.poem.PoemDatabaseFactory;
+import org.melati.poem.SQLSeriousPoemException;
+import org.melati.poem.dbms.test.ThrowingResultSet;
+
+public class CachedSelectionTest extends
+        org.melati.poem.test.CachedSelectionTest {
+
+  public CachedSelectionTest() {
+  }
+
+  public CachedSelectionTest(String name) {
+    super(name);
+  }
+  protected void setUp() throws Exception {
+    PoemDatabaseFactory.removeDatabase(databaseName);
+    super.setUp();
+    assertEquals("org.melati.poem.dbms.test.HsqldbThrower",getDb().getDbms().getClass().getName());
+  }
+  
+  public Database getDatabase(String name) {
+    maxTrans = 4;
+    Database db = PoemDatabaseFactory.getDatabase(name, 
+        "jdbc:hsqldb:mem:" + name,
+        "sa", 
+        "",
+        "org.melati.poem.PoemDatabase",
+        "org.melati.poem.dbms.test.HsqldbThrower", 
+        false, 
+        false, 
+        false, maxTrans);
+    return db;
+  }
+
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testFirstObject() {
+    ThrowingResultSet.startThrowing("close");
+    ThrowingResultSet.startThrowing("next");
+    try { 
+      super.testFirstObject();
+      fail("Should have bombed");
+    } catch (SQLSeriousPoemException e) { 
+      assertEquals("ResultSet bombed", e.innermostException().getMessage());      
+    }
+    ThrowingResultSet.stopThrowing("close");
+    ThrowingResultSet.stopThrowing("next");
+
+  }
+
+  public void testMultiTableSelection() {
+  }
+
+  public void testNth() {
+  }
+
+  public void testToString() {
+  }
+
+}

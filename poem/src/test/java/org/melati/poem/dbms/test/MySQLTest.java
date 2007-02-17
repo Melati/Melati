cee609dde2e86e15de1e2a8e831a80ceb36d867f
Tim Pizey <timp@paneris.org>
2007-02-17 20:07:08 +0000

    Test LIKE

--- poem/src/test/java/org/melati/poem/dbms/test/MySQLTest.java
+++ poem/src/test/java/org/melati/poem/dbms/test/MySQLTest.java
@@ -177,6 +177,29 @@ public class MySQLTest extends DbmsSpec {
     assertEquals(expected, actual);
 
   }
+  /**
+   * Test method for {@link org.melati.poem.dbms.Dbms#
+   * caseInsensitiveRegExpSQL(java.lang.String, java.lang.String)}.
+   */
+  public void testCaseInsensitiveRegExpSQL() {
+    String expected = "a LIKE '%b%'";
+    String actual = it.caseInsensitiveRegExpSQL("a", "b");
+    assertEquals(expected, actual);
+  }
+
+  public void testCaseInsensitiveRegExpSQLQuoted() {
+    String expected = "a LIKE '%b%'";
+    String actual = it.caseInsensitiveRegExpSQL("a", "\"b\"");
+    assertEquals(expected, actual);
+  }
+
+  public void testCaseInsensitiveRegExpSQLBlank() {
+    String expected = " LIKE '%%'";
+    String actual = it.caseInsensitiveRegExpSQL("", "");
+    assertEquals(expected, actual);
+  }
+
+  
 
 
 }

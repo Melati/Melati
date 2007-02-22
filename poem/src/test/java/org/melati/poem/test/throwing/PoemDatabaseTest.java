f4827391c5f346f49017bf7bd14027297347f7a2
Tim Pizey <timp@paneris.org>
2007-02-22 20:48:12 +0000

    No unmodified re runs of extended tests

--- poem/src/test/java/org/melati/poem/test/throwing/PoemDatabaseTest.java
+++ poem/src/test/java/org/melati/poem/test/throwing/PoemDatabaseTest.java
@@ -48,255 +48,255 @@ public class PoemDatabaseTest extends org.melati.poem.test.PoemDatabaseTest {
   }
 
   public void testAddConstraints() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testAddConstraints();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testAddConstraints();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testAdministerCapability() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testAdministerCapability();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testAdministerCapability();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testBeginExclusiveLock() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testBeginExclusiveLock();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testBeginExclusiveLock();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testBeginSession() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testBeginSession();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testBeginSession();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testColumns() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testColumns();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testColumns();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testConnect() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testConnect();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testConnect();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testDisconnect() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testDisconnect();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testDisconnect();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testDump() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testDump();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testDump();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testDumpCacheAnalysis() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testDumpCacheAnalysis();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testDumpCacheAnalysis();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testEndExclusiveLock() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testEndExclusiveLock();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testEndExclusiveLock();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testEndSession() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testEndSession();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testEndSession();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetCanAdminister() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetCanAdminister();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetCanAdminister();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetCapabilityTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetCapabilityTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetCapabilityTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetColumnInfoTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetColumnInfoTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetColumnInfoTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetCommittedConnection() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetCommittedConnection();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetCommittedConnection();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetDbms() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetDbms();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetDbms();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetDisplayTables() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetDisplayTables();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetDisplayTables();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetFreeTransactionsCount() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetFreeTransactionsCount();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetFreeTransactionsCount();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetGroupCapabilityTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetGroupCapabilityTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetGroupCapabilityTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetGroupMembershipTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetGroupMembershipTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetGroupMembershipTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetGroupTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetGroupTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetGroupTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetSettingTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetSettingTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetSettingTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetTableCategoryTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetTableCategoryTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetTableCategoryTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetTableInfoTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetTableInfoTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetTableInfoTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetTransactionsCount() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetTransactionsCount();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetTransactionsCount();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGetUserTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGetUserTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGetUserTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGivesCapabilitySQL() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGivesCapabilitySQL();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGivesCapabilitySQL();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testGuestAccessToken() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testGuestAccessToken();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testGuestAccessToken();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testHasCapability() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testHasCapability();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testHasCapability();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testInSession() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testInSession();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testInSession();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testIsFree() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testIsFree();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testIsFree();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testLogCommits() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testLogCommits();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testLogCommits();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testLogSQL() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testLogSQL();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testLogSQL();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testPoemTransaction() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testPoemTransaction();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testPoemTransaction();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testQuotedName() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testQuotedName();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testQuotedName();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testReferencesToPersistent() {
@@ -311,10 +311,10 @@ public class PoemDatabaseTest extends org.melati.poem.test.PoemDatabaseTest {
   }
 
   public void testReferencesToTable() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testReferencesToTable();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testReferencesToTable();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSetCanAdminister() {
@@ -339,101 +339,101 @@ public class PoemDatabaseTest extends org.melati.poem.test.PoemDatabaseTest {
   }
 
   public void testSetLogCommits() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSetLogCommits();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSetLogCommits();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSetLogSQL() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSetLogSQL();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSetLogSQL();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSetTransactionsMax() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSetTransactionsMax();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSetTransactionsMax();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testShutdown() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testShutdown();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testShutdown();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSqlQuery() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSqlQuery();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSqlQuery();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSqlUpdate() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSqlUpdate();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSqlUpdate();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSqlUpdate1() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSqlUpdate1();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSqlUpdate1();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testSqlUpdate2() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testSqlUpdate2();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testSqlUpdate2();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testTables() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testTables();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testTables();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testToString() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testToString();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testToString();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testTransaction() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testTransaction();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testTransaction();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testTransactionsMax() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testTransactionsMax();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testTransactionsMax();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testTrimCache() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testTrimCache();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testTrimCache();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
   public void testUncacheContents() {
-    ThrowingConnection.startThrowing("prepareStatement");
+    // ThrowingConnection.startThrowing("prepareStatement");
 
-    super.testUncacheContents();
-    ThrowingConnection.stopThrowing("prepareStatement");
+    // super.testUncacheContents();
+    // ThrowingConnection.stopThrowing("prepareStatement");
   }
 
 }

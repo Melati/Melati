61ac24d7b21e840f1dcf74f69246b7eac528a0a2
Tim Pizey <timp@paneris.org>
2007-05-29 15:12:27 +0000

    Initial version

---
+++ poem/src/test/java/org/melati/poem/util/test/ArrayEnumerationTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package org.melati.poem.util.test;
+
+import java.util.NoSuchElementException;
+
+import org.melati.poem.util.ArrayEnumeration;
+
+import junit.framework.TestCase;
+
+/**
+ * @author timp
+ * @since 29 May 2007
+ *
+ */
+public class ArrayEnumerationTest extends TestCase {
+
+  /**
+   * @param name
+   */
+  public ArrayEnumerationTest(String name) {
+    super(name);
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.ArrayEnumeration#ArrayEnumeration(java.lang.Object[])}.
+   */
+  public void testArrayEnumeration() {
+    
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.ArrayEnumeration#hasMoreElements()}.
+   */
+  public void testHasMoreElements() {
+    
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.ArrayEnumeration#nextElement()}.
+   */
+  public void testNextElement() {
+    String[] them = new String[] {"one", "two", "three"};
+    ArrayEnumeration it = new ArrayEnumeration(them);
+    while (it.hasMoreElements()) 
+      it.nextElement();
+    try { 
+      it.nextElement();
+      fail("should have bombed");
+    } catch (NoSuchElementException e) {
+      e = null;      
+    }
+  }
+
+}
---
+++ poem/src/test/java/org/melati/poem/util/test/EmptyEnumerationTest.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.melati.poem.util.test;
+
+import java.util.NoSuchElementException;
+
+import org.melati.poem.util.EmptyEnumeration;
+
+import junit.framework.TestCase;
+
+/**
+ * @author timp
+ * @since 29 May 2007
+ *
+ */
+public class EmptyEnumerationTest extends TestCase {
+
+  /**
+   * @param name
+   */
+  public EmptyEnumerationTest(String name) {
+    super(name);
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.EmptyEnumeration#hasMoreElements()}.
+   */
+  public void testHasMoreElements() {
+    assertFalse(EmptyEnumeration.it.hasMoreElements());
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.EmptyEnumeration#nextElement()}.
+   */
+  public void testNextElement() {
+    try { 
+      EmptyEnumeration.it.nextElement();
+      fail("should have bombed");
+    } catch (NoSuchElementException e) {
+      e = null;      
+    }
+  }
+
+}

360a2afce99d55de74c16c73d2d429261c9ba979
Tim Pizey <timp@paneris.org>
2007-06-14 08:44:40 +0000

    Unify comments and copyright with current webmacro

--- poem/src/main/java/org/melati/poem/util/Base64.java
+++ poem/src/main/java/org/melati/poem/util/Base64.java
@@ -1,25 +1,29 @@
 /*
- * Copyright (c) 1998, 1999 Semiotek Inc. All Rights Reserved.
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.
  *
- * This software is the confidential intellectual property of
- * of Semiotek Inc.; it is copyrighted and licensed, not sold.
- * You may use it under the terms of the GNU General Public License,
- * version 2, as published by the Free Software Foundation. If you 
- * do not want to use the GPL, you may still use the software after
- * purchasing a proprietary developers license from Semiotek Inc.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of either of the following
+ * Open Source licenses:
+ *
+ * The GNU General Public License, version 2, or any later version, as
+ * published by the Free Software Foundation
+ * (http://www.fsf.org/copyleft/gpl.html);
+ *
+ *  or
+ *
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)
  *
  * This software is provided "as is", with NO WARRANTY, not even the 
  * implied warranties of fitness to purpose, or merchantability. You
  * assume all risks and liabilities associated with its use.
  *
- * See the attached License.html file for details, or contact us
- * by e-mail at info@semiotek.com to get a copy.
+ * See www.webmacro.org for more information on the WebMacro project.
  */
 
 package org.melati.poem.util;
 
 /**
- * A representation of the Base64 encoding scheme.
+ * A set of utility methods for working with Base64.
  *
  */
 
@@ -138,7 +142,6 @@ public final class Base64 {
       byte ptext;
       byte sixbits;
       int ppos = 0; 
-//      int epos = 0;
       boolean cutShort = false;
       while (ppos < plain.length) {
          // first char: upper 6 bytes

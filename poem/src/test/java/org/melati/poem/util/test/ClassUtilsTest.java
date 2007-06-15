b1fc80ef3116a28eef7f18455bf2532c52e31051
Tim Pizey <timp@paneris.org>
2007-06-15 15:02:05 +0000

    Initial empty version

---
+++ poem/src/test/java/org/melati/poem/util/test/ClassUtilsTest.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package org.melati.poem.util.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author timp
+ * @since 15 Jun 2007
+ *
+ */
+public class ClassUtilsTest extends TestCase {
+
+  /**
+   * @param name
+   */
+  public ClassUtilsTest(String name) {
+    super(name);
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.ClassUtils#getOneArgumentMethods(java.lang.Object, java.lang.String)}.
+   */
+  public void testSuitableSetterMethods() {
+    
+  }
+
+  /**
+   * Test method for {@link org.melati.poem.util.ClassUtils#getNoArgMethod(java.lang.Object, java.lang.String)}.
+   */
+  public void testSuitableGetterMethods() {
+    
+  }
+
+}

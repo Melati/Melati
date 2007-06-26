f1c0b66b303b695840a7f187a6ee9282bf7c1c0b
Tim Pizey <timp@paneris.org>
2007-06-26 15:18:02 +0000

    Velocity throws a MelatiBugMelatiException

--- melati/src/test/java/org/melati/template/test/HTMLMarkupLanguageVelocityTest.java
+++ melati/src/test/java/org/melati/template/test/HTMLMarkupLanguageVelocityTest.java
@@ -2,6 +2,7 @@ package org.melati.template.test;
 
 import org.melati.MelatiConfig;
 import org.melati.template.velocity.VelocityTemplateEngine;
+import org.melati.util.MelatiBugMelatiException;
 import org.melati.util.MelatiException;
 
 
@@ -23,6 +24,19 @@ public class HTMLMarkupLanguageVelocityTest extends HTMLMarkupLanguageTest {
   protected void melatiConfig() throws MelatiException {
     mc = new MelatiConfig();
     mc.setTemplateEngine(new VelocityTemplateEngine());
+  }
+
+  /**
+   * Test that a syntax error in a WM templet is handled by Velocity.
+   */
+  public void testSyntaxErrorInWMTemplet() throws Exception { 
+    Object templated = new TemplatedWithWMSyntaxError();
+    try { 
+      ml.rendered(templated);
+      fail("Should have bombed");
+    } catch (MelatiBugMelatiException e) { 
+      e = null;
+    }
   }
   
 }

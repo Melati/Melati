df91f262bc354c815a7ef9438079afce44cb0cf6
Tim Pizey <timp@paneris.org>
2008-01-31 11:15:06 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/util/test/MD5UtilTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.melati.util.test;
+
+import org.apache.commons.codec.binary.Base64;
+import org.melati.util.MD5Util;
+
+import junit.framework.TestCase;
+
+/**
+ * @author timp
+ * @since 31 Jan 2008
+ *
+ */
+public class MD5UtilTest extends TestCase {
+
+  /**
+   * @param name
+   */
+  public MD5UtilTest(String name) {
+    super(name);
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link org.melati.util.MD5Util#encode(java.lang.String)}.
+   */
+  public void testEncode() {
+    String in = "FIXME";
+    assertEquals(new String(Base64.encodeBase64(MD5Util.encode(in).getBytes())), 
+            "VtdpXKhwRAitMduH224/7A==");
+  }
+
+}

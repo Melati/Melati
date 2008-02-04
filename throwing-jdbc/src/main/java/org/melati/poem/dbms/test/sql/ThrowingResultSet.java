6766e1882d111b27c1ce8b6d4d46bd220387612f
Tim Pizey <timp@paneris.org>
2008-02-04 22:44:35 +0000

    Add dummy 1.6 method

--- throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingResultSet.java
+++ throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingResultSet.java
@@ -1613,25 +1613,5 @@ public class ThrowingResultSet extends Thrower implements ResultSet {
     is = is2;    
   }
 
-  /**
-   * 
-   */
-  public void updateNCharacterStream(int i, Reader r){
-    int i2 = i;
-    i = i2;
-    Reader r2 = r;
-    r = r2;    
-  }
-
-  /**
-   * 
-   */
-  public void updateNCharacterStream(String s, Reader r){
-    String s2 = s;
-    s = s2;
-    Reader r2 = r;
-    r = r2;    
-  }
-
 
 }

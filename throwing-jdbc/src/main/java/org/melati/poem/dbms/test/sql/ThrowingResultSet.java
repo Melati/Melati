47952f580fed3ee035090119a772d55ff6e0587a
Tim Pizey <timp@paneris.org>
2008-02-04 23:21:46 +0000

    Add dummy 1.6 method

--- throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingResultSet.java
+++ throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingResultSet.java
@@ -1648,6 +1648,17 @@ public class ThrowingResultSet extends Thrower implements ResultSet {
   /**
    * 
    */
+  public void setCharacterStream(int i, Reader r, long l){
+    int i2 = i;
+    i = i2;
+    Reader r2 = r;
+    r = r2;
+    long l2 = l;
+    l = l2;
+  }
+  /**
+   * 
+   */
   public void setCharacterStream(String s, Reader r){
     String s2 = s;
     s = s2;
@@ -1673,6 +1684,18 @@ public class ThrowingResultSet extends Thrower implements ResultSet {
     s = s2;
     InputStream is2 = is;
     is = is2;    
+  }
+
+  /**
+   * 
+   */
+  public void updateBinaryStream(String s, InputStream is, long l){
+    String s2 = s;
+    s = s2;
+    InputStream is2 = is;
+    is = is2;
+    long l2 = l;
+    l = l2;
   }
 
   /**

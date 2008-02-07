183221c2d4a453be9784dc4870aa2134cc443037
Tim Pizey <timp@paneris.org>
2008-02-07 19:55:23 +0000

    Oops

--- throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingBlob.java
+++ throwing-jdbc/src/main/java/org/melati/poem/dbms/test/sql/ThrowingBlob.java
@@ -44,10 +44,7 @@
 
 package org.melati.poem.dbms.test.sql;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.sql.Blob;
-import java.sql.SQLException;
 
 /**
  * @author timp
@@ -64,95 +61,5 @@ public class ThrowingBlob extends ThrowingBlobVariant implements Blob {
     it = b;
   }
 
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#getBinaryStream()
-   */
-  public InputStream getBinaryStream() throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "getBinaryStream"))
-      throw new SQLException("Blob bombed");
-    return it.getBinaryStream();
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#getBytes(long, int)
-   */
-  public byte[] getBytes(long pos, int length) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "getBytes"))
-      throw new SQLException("Blob bombed");
-    return it.getBytes(pos, length);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#length()
-   */
-  public long length() throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "length"))
-      throw new SQLException("Blob bombed");
-    return it.length();
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#position(byte[], long)
-   */
-  public long position(byte[] pattern, long start) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "position"))
-      throw new SQLException("Blob bombed");
-    return it.position(pattern, start);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#position(java.sql.Blob, long)
-   */
-  public long position(Blob pattern, long start) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "position"))
-      throw new SQLException("Blob bombed");
-    return it.position(pattern, start);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#setBinaryStream(long)
-   */
-  public OutputStream setBinaryStream(long pos) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "setBinaryStream"))
-      throw new SQLException("Blob bombed");
-    return it.setBinaryStream(pos);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#setBytes(long, byte[])
-   */
-  public int setBytes(long pos, byte[] bytes) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "setBytes"))
-      throw new SQLException("Blob bombed");
-    return it.setBytes(pos, bytes);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#setBytes(long, byte[], int, int)
-   */
-  public int setBytes(long pos, byte[] bytes, int offset, int len)
-          throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "setBytes"))
-      throw new SQLException("Blob bombed");
-    return it.setBytes(pos, bytes, offset, len);
-  }
-
-  /** 
-   * {@inheritDoc}
-   * @see java.sql.Blob#truncate(long)
-   */
-  public void truncate(long len) throws SQLException {
-    if (shouldThrow(this.getClass().getInterfaces()[0], "truncate"))
-      throw new SQLException("Blob bombed");
-    it.truncate(len);
-  }
 
 }

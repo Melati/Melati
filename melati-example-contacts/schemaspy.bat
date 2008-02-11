2738884727077ab24c3cf5df1158c3fe0fcbd395
Tim Pizey <timp@paneris.org>
2008-02-11 12:42:18 +0000

    Working

---
+++ melati-example-contacts/schemaspy.bat
@@ -0,0 +1,9 @@
+java -jar schemaSpy_3.1.1.jar  ^
+     -cp hsqldb-1.8.0.7.jar ^
+     -t src/site/schemaspy/properties/hsqldb-file.properties  ^
+     -db contacts ^
+     -dir /dist/melati/db ^
+     -u sa ^
+     -s PUBLIC ^
+     -o out 
+

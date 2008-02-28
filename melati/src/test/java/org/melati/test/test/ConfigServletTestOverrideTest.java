ae0942009781bce7e36f3a420ac469e26a3c5d7b
Tim Pizey <timp@paneris.org>
2008-02-28 20:59:01 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/test/test/ConfigServletTestOverrideTest.java
@@ -0,0 +1,93 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2008 Tim Pizey
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Tim Pizey <timp At paneris.org>
+ *     http://paneris.org/~timp
+ */
+
+package org.melati.test.test;
+
+/**
+ * @author timp
+ * @since  28 Feb 2008
+ *
+ */
+public class ConfigServletTestOverrideTest extends ConfigServletTestTest{
+
+  /**
+   * Constructor.
+   * @param name
+   */
+  public ConfigServletTestOverrideTest(String name) {
+    super(name);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.melati.JettyWebTestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.melati.JettyWebTestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void testUpload() { 
+    setScriptingEnabled(false);
+    beginAt("/org.melati.test.ConfigServletTestOverride");
+    assertTextPresent("DefaultFileDataAdaptorFactory");
+    assertTextPresent("org.melati.servlet.DefaultFileDataAdaptorFactory");
+    setTextField("file","/dist/melati/melati/src/main/java/org/melati/admin/static/file.gif");
+    submit();
+    assertWindowPresent("Upload");
+    setTextField("file","/dist/melati/LICENSE-GPL.txt");
+    submit();
+    gotoWindow("Upload");
+    assertTextPresent("GNU GENERAL PUBLIC LICENSE");
+    
+  }
+
+}

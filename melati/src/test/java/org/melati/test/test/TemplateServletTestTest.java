28a678c5ead9ae4c58e61901a21dd55d642abd3c
Tim Pizey <timp@paneris.org>
2008-03-07 09:14:30 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/test/test/TemplateServletTestTest.java
@@ -0,0 +1,127 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2008 Tim Pizey
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Tim Pizey <timp At paneris.org>
+ *     http://paneris.org/~timp
+ */
+package org.melati.test.test;
+
+import org.melati.JettyWebTestCase;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+
+/**
+ * @author timp
+ * @since 7 Mar 2008
+ *
+ */
+public class TemplateServletTestTest extends JettyWebTestCase {
+
+  /**
+   * @param name
+   */
+  public TemplateServletTestTest(String name) {
+    super(name);
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /** 
+   * {@inheritDoc}
+   * @see junit.framework.TestCase#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Click Exception link.
+   */
+  public void testException() {
+    setScriptingEnabled(false);
+    beginAt("/org.melati.test.PoemServletTest/melatitest");
+    clickLinkWithText("Exception");
+    assertTextPresent("MelatiBugMelatiException");
+  }
+  /**
+   * Click Exception link.
+   */
+  public void testAccessException() {
+    setScriptingEnabled(false);
+    beginAt("/org.melati.test.PoemServletTest/melatitest");
+    try { 
+      clickLinkWithText("Access Poem Exception");
+      fail("Should have bombed.");
+    } catch (FailingHttpStatusCodeException e) { 
+      System.err.println(e.getMessage());
+      assertEquals(e.getMessage().indexOf("401"), 0);
+    }
+    assertTextPresent("Error 401");
+    assertTextPresent("You need the capability _administer_ ");
+  }
+  /**
+   * Fill and click upload.
+   */
+  public void BorkedTestUpload() { 
+    setScriptingEnabled(false);
+    beginAt("/org.melati.login.Login/admintest");
+    setTextField("field_login", "_administrator_");
+    setTextField("field_password", "FIXME");
+    checkCheckbox("rememberme");
+    submit();
+    gotoPage("/org.melati.test.PoemServletTest/admintest/");
+    setTextField("file","/dist/melati/melati/src/main/java/org/melati/admin/static/file.gif");
+    submit();
+    assertWindowPresent("Upload");
+    setTextField("file","/dist/melati/LICENSE-GPL.txt");
+    submit();
+    gotoWindow("Upload");
+    assertTextPresent("GNU GENERAL PUBLIC LICENSE");
+    
+  }
+
+  
+}

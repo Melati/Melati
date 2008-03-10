48bff4f868a3d6e4a78d9f83b3e1aebc1ba0eafe
Tim Pizey <timp@paneris.org>
2008-03-10 22:26:49 +0000

    Regig exception handling

--- melati/src/main/java/org/melati/test/WebmacroStandalone.java
+++ melati/src/main/java/org/melati/test/WebmacroStandalone.java
@@ -21,12 +21,15 @@
 
 package org.melati.test;
 
+import org.webmacro.ResourceException;
 import org.webmacro.WebMacro;
 import org.webmacro.WM;
 import org.melati.template.webmacro.FastWriter;
 import org.webmacro.Template;
 import org.webmacro.InitException;
 import org.webmacro.servlet.WebContext;
+
+import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -36,157 +39,132 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
 /**
- * This example demonstrates using WebMacro in "standalone" mode. 
- * Instead of subclassing from WMServlet you create and maintain 
- * your own WebMacro object
- * and you are free to subclass from another servlet. 
- * Also, this technique can be used outside the servlet context.
+ * This example demonstrates using WebMacro in "standalone" mode. Instead of
+ * subclassing from WMServlet you create and maintain your own WebMacro object
+ * and you are free to subclass from another servlet. Also, this technique can
+ * be used outside the servlet context.
  * <p>
- * The WebMacro master object is initialized when the servlet is initialized
- * and destroyed when the servlet is destroyed. There is some overhead 
- * involved in creating the interface so you should prefer not to create one
- * on every request, although it is not too expensive. 
+ * The WebMacro master object is initialized when the servlet is initialized and
+ * destroyed when the servlet is destroyed. There is some overhead involved in
+ * creating the interface so you should prefer not to create one on every
+ * request, although it is not too expensive.
  * <p>
- * This servlet can be compiled and installed as an ordinary servlet. You 
- * need to ensure that your WebMacro.properties file is properly configured
- * and available on your CLASSPATH. When setting up WebMacro.properties 
- * make sure that the TemplatePath is correctly set and that the template
- * used by this servlet, "standalone.wm", is available on that path.
+ * This servlet can be compiled and installed as an ordinary servlet. You need
+ * to ensure that your WebMacro.properties file is properly configured and
+ * available on your CLASSPATH. When setting up WebMacro.properties make sure
+ * that the TemplatePath is correctly set and that the template used by this
+ * servlet, "standalone.wm", is available on that path.
  */
 public class WebmacroStandalone extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-   /**
-     * This is the core WebMacro interface which we use to create Contexts, 
-     * load Templates, and begin other WebMacro operations.
-     */
-   private WebMacro _wm = null;
+  /**
+   * This is the core WebMacro interface which we use to create Contexts, load
+   * Templates, and begin other WebMacro operations.
+   */
+  private WebMacro _wm = null;
 
-   /**
-     * The init() method will be called by your servlet runner whenever 
-     * it wants to instantiate a new copy of your servlet. We initialize
-     * WebMacro here so as to avoid the expense of initializing it on 
-     * every request. 
-     */
-   public void init(ServletConfig sc) throws ServletException {
+  /**
+   * The init() method will be called by your servlet runner whenever it wants
+   * to instantiate a new copy of your servlet. We initialize WebMacro here so
+   * as to avoid the expense of initializing it on every request.
+   */
+  public void init(ServletConfig sc) throws ServletException {
+    try {
+      if (_wm == null) {
+        _wm = new WM();
+      }
+    } catch (InitException e) {
+      throw new ServletException("Could not initialize WebMacro: " + e);
+    }
+  }
+
+  /**
+   * It's not strictly necessary to destroy the WebMacro object when you're done
+   * with it--garbage collection will eventually get it--but it makes sure the
+   * resources get freed. You really ought to do this, since it might be a lot
+   * of resources.
+   */
+  public void destroy() {
+    if (_wm != null) {
+      _wm = null;
+    }
+  }
+
+  /**
+   * We only implement the GET method in this servlet.
+   * <p>
+   * We create a WebContext object, populate it with some data, select a
+   * template, and write the output of that template to the output stream. Note
+   * that you need to pass the WebContext object to the template in order to
+   * write it, you also need an output stream, in this case the
+   * HttpServletResponse output stream is used.
+   * <p>
+   * If you were using WebMacro outside a servlet context you would not be able
+   * to construct a WebContext object, since you would not have the
+   * HttpServletRequest and HttpServletResponse objects which you need in order
+   * to do that. WebContext is a subclass of Context, so outside of a servlet
+   * just construct a TemplateContext object. You will obviously lose the
+   * ability to talk about servlet specific things in your templates (such as
+   * "Cookies") but otherwise it's the same.
+   * <p>
+   * There are a few exceptions you have to deal with. First, WebMacro may not
+   * be able to locate the template you've requested in which case it'll throw a
+   * NotFoundException. Second, the template will expect to find certain
+   * information in the TemplateContext, and if you fail to provide that
+   * information a ContextException will be thrown. Aside than WebMacro, in a
+   * servlet environment you also have to deal with the IOException that can be
+   * thrown if something goes wrong with the network connection back to the
+   * client.
+   * @throws IOException 
+   * @throws IOException 
+   */
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String templateName = "org/melati/test/WebmacroStandalone.wm";
+
+      WebContext c = new WebContext(_wm.getBroker(), req, resp);
+
+      // fill up the context with our data
+      c.put("Today", new Date());
+      c.put("Number", new Long(23));
+
+      // WebContext provides some utilities as well
+      String other = c.getForm("other");
+      if (other == null) {
+        c.put("hello", "hello again!"); // put this into the hash
+      } else {
+        c.put("hello", other); // else put this in
+      }
+
+      FastWriter fw = new FastWriter(_wm.getBroker(), resp.getOutputStream(),
+          resp.getCharacterEncoding());
+        // get the template we intend to execute
+      Template t = null;
       try {
-         if (_wm == null) {
-            _wm = new WM();
-         }
-      } catch (InitException e) {
-         throw new ServletException("Could not initialize WebMacro: " + e);
-      }
-   }
+        t = _wm.getTemplate(templateName);
+      } catch (org.webmacro.NotFoundException e) {
 
-   /**
-     * It's not strictly necessary to destroy the WebMacro object when 
-     * you're done with it--garbage collection will eventually get it--but
-     * it makes sure the resources get freed. You really ought to do this,
-     * since it might be a lot of resources. 
-     */
-   public void destroy() {
-      if (_wm != null) {
-         _wm = null;
-      }
-   }
-
-
-   /**
-     * We only implement the GET method in this servlet. 
-     * <p>
-     * We create a WebContext object, populate it with some data, select
-     * a template, and write the output of that template to the output 
-     * stream. Note that you need to pass the WebContext object to the 
-     * template in order to write it, you also need an output stream, in
-     * this case the HttpServletResponse output stream is used. 
-     * <p>
-     * If you were using WebMacro outside a servlet context you would not
-     * be able to construct a WebContext object, since you would not have 
-     * the HttpServletRequest and HttpServletResponse objects which you 
-     * need in order to do that. WebContext is a subclass of Context, so 
-     * outside of a servlet just construct a TemplateContext object. You will 
-     * obviously lose the ability to talk about servlet specific things 
-     * in your templates (such as "Cookies") but otherwise it's the same.
-     * <p>
-     * There are a few exceptions you have to deal with. First, WebMacro 
-     * may not be able to locate the template you've requested in which 
-     * case it'll throw a NotFoundException. Second, the template will 
-     * expect to find certain information in the TemplateContext, and if you fail 
-     * to provide that information a ContextException will be thrown. Aside
-     * than WebMacro, in a servlet environment you also have to deal with 
-     * the IOException that can be thrown if something goes wrong with 
-     * the network connection back to the client.
-     */
-   public void doGet(HttpServletRequest req, HttpServletResponse resp) {
-      String templateName = "org/melati/test/WebmacroStandalone.wm";
+        fw.write("ERROR!  Could not locate template " + templateName
+            + ", check that your template path is set "
+            + "properly in WebMacro.properties");
+        e.printStackTrace();
+        fw.close();
+        return;        
+      } catch (ResourceException e) {
+        fw.write("ERROR! "
+            + "Could not locate required Resource in the TemplateContext.");
+        e.printStackTrace();
+        return;        
+      } 
       try {
-         try {
-
-            WebContext c = new WebContext(_wm.getBroker(), req, resp);
-
-            // fill up the context with our data
-            c.put("Today", new Date());
-            c.put("Number", new Long(23));
-
-            // WebContext provides some utilities as well
-            String other = c.getForm("other"); 
-            if (other == null) {
-               c.put("hello","hello again!"); // put this into the hash
-            } else {
-               c.put("hello",other);          // else put this in
-            }
-
-            // get the template we intend to execute
-            Template t = _wm.getTemplate(templateName);
-
-            // Create FastWriter for fast output encoding
-//            FastWriter fw = new FastWriter(resp.getOutputStream(),
-//                                           resp.getCharacterEncoding());
-            FastWriter fw = new FastWriter(_wm.getBroker(),
-                                           resp.getOutputStream(),
-                                           resp.getCharacterEncoding());
-            // write the template to the output, using our context
-            t.write(fw.getOutputStream(), c);
-            fw.close();
-         } catch (org.webmacro.NotFoundException e) {
-            FastWriter out = new FastWriter(_wm.getBroker(),
-                                           resp.getOutputStream(),
-                                           resp.getCharacterEncoding());
-//             FastWriter out = new FastWriter(resp.getOutputStream(),
-//                                             resp.getCharacterEncoding());
-             
-             out.write("ERROR!  Could not locate template " +
-                       templateName + 
-                       ", check that your template path is set " +
-                       "properly in WebMacro.properties");
-             out.close();
-         } catch (org.webmacro.ContextException e) {
-            FastWriter out = new FastWriter(_wm.getBroker(),
-                                           resp.getOutputStream(),
-                                           resp.getCharacterEncoding());
-//             FastWriter out = new FastWriter(resp.getOutputStream(),
-//                                             resp.getCharacterEncoding());
-             out.write("ERROR! " + 
-                       "Could not locate required data in the TemplateContext.");
-             e.printStackTrace();
-             out.close();
-         } catch (org.webmacro.ResourceException e) {
-            FastWriter out = new FastWriter(_wm.getBroker(),
-                                           resp.getOutputStream(),
-                                           resp.getCharacterEncoding());
-//             FastWriter out = new FastWriter(resp.getOutputStream(),
-//                                             resp.getCharacterEncoding());
-             out.write("ERROR! " + 
-                       "Could not locate required Resource in the TemplateContext.");
-             e.printStackTrace();
-             out.close();
-         }
-      } catch (java.io.IOException e) {
-          // what else can we do?
-          System.out.println("ERROR: " + 
-                             "IOException writing to servlet output stream.");
-          e.printStackTrace();
-      }
-   }
+        // write the template to the output, using our context
+        t.write(fw.getOutputStream(), c);
+      } catch (org.webmacro.ContextException e) {
+        fw.write("ERROR! "
+            + "Could not locate required data in the TemplateContext.");
+        e.printStackTrace();
+      } 
+      fw.close();
+  }
 
 }

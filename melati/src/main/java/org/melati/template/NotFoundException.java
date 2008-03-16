e06b19e38e904f517097702dcfc4c3e9e8b9e6fa
Tim Pizey <timp@paneris.org>
2008-03-16 18:38:46 +0000

    Add constructor

--- melati/src/main/java/org/melati/template/NotFoundException.java
+++ melati/src/main/java/org/melati/template/NotFoundException.java
@@ -38,7 +38,7 @@
  *
  * Contact details for copyright holder:
  *
- *     Tim Joyce <timj@paneris.org>
+ *     Tim Joyce <timj At paneris.org>
  *     http://paneris.org/
  *     68 Sandbanks Rd, Poole, Dorset. BH14 8BY. UK
  */
@@ -69,6 +69,9 @@ public class NotFoundException extends MelatiException {
   public NotFoundException(Exception underlying) {
     super(underlying);
     message = underlying.toString();
+  }
+  public NotFoundException(String message, Exception underlying) {
+    super(message, underlying);
   }
 
   /** Constructor. */

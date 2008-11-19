d7cbd6422b2dfa386c8a12647880a58378a6a694
Tim Pizey <timp@paneris.org>
2008-11-19 20:10:37 +0000

    Initial version

---
+++ melati/src/test/java/org/melati/admin/test/UploadedImage.java
@@ -0,0 +1,42 @@
+package org.melati.admin.test;
+
+import org.melati.admin.test.generated.UploadedImageBase;
+
+/**
+ * Melati POEM generated, programmer modifiable stub 
+ * for a <code>Persistent</code> <code>UploadedImage</code> object.
+ * 
+ * <p> 
+ * Description: 
+ *   An image uploaded by a user. 
+ * </p>
+ * 
+ * <table> 
+ * <tr><th colspan='3'>
+ * Field summary for SQL table <code>UploadedImage</code>
+ * </th></tr>
+ * <tr><th>Name</th><th>Type</th><th>Description</th></tr>
+ * <tr><td> id </td><td> Integer </td><td> &nbsp; </td></tr> 
+ * <tr><td> filename </td><td> String </td><td> The path name of the file, 
+ * relative to the server root </td></tr> 
+ * </table> 
+ * 
+ * @see org.melati.poem.prepro.TableDef#generatePersistentJava 
+ */
+public class UploadedImage extends UploadedImageBase {
+
+ /**
+  * Constructor 
+  * for a <code>Persistent</code> <code>UploadedImage</code> object.
+  * <p>
+  * Description: 
+  *   An image uploaded by a user. 
+  * </p>
+  * 
+  * @see org.melati.poem.prepro.TableDef#generatePersistentJava 
+  */
+  public UploadedImage() { }
+
+  // programmer's domain-specific code here
+}
+
---
+++ melati/src/test/java/org/melati/admin/test/UploadedImageTable.java
@@ -0,0 +1,48 @@
+package org.melati.admin.test;
+
+import org.melati.admin.test.generated.UploadedImageTableBase;
+import org.melati.poem.DefinitionSource;
+import org.melati.poem.Database;
+import org.melati.poem.PoemException;
+
+/**
+ * Melati POEM generated, programmer modifiable stub 
+ * for a <code>UploadedImageTable</code> object.
+ * <p>
+ * Description: 
+ *   An image uploaded by a user. 
+ * </p>
+ *
+ * 
+ * <table> 
+ * <tr><th colspan='3'>
+ * Field summary for SQL table <code>UploadedImage</code>
+ * </th></tr>
+ * <tr><th>Name</th><th>Type</th><th>Description</th></tr>
+ * <tr><td> id </td><td> Integer </td><td> &nbsp; </td></tr> 
+ * <tr><td> filename </td><td> String </td><td> The path name of the file, 
+ * relative to the server root </td></tr> 
+ * </table> 
+ * 
+ * @see  org.melati.poem.prepro.TableDef#generateTableJava 
+ */
+public class UploadedImageTable extends UploadedImageTableBase {
+
+ /**
+  * Constructor.
+  * 
+  * @see org.melati.poem.prepro.TableDef#generateTableJava 
+  * @param database          the POEM database we are using
+  * @param name              the name of this <code>Table</code>
+  * @param definitionSource  which definition is being used
+  * @throws PoemException    if anything goes wrong
+  */
+  public UploadedImageTable(
+      Database database, String name,
+      DefinitionSource definitionSource) throws PoemException {
+    super(database, name, definitionSource);
+  }
+
+  // programmer's domain-specific code here
+}
+

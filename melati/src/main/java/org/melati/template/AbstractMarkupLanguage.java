34c1c5b1d15d720cc22cd7e7c0ca906a26eeaed5
Tim Pizey <timp@paneris.org>
2009-02-25 23:20:56 +0000

    Remove unreachable code

--- melati/src/main/java/org/melati/template/AbstractMarkupLanguage.java
+++ melati/src/main/java/org/melati/template/AbstractMarkupLanguage.java
@@ -308,8 +308,6 @@ public abstract class AbstractMarkupLanguage implements MarkupLanguage {
         Template templet =
           templetLoader.templet(melati.getTemplateEngine(), this, o.getClass());
         vars.put("object", o);
-        // Not happy but 
-        if (o instanceof Field) vars.put("field", o);
         vars.put("melati", melati);
         vars.put("ml", melati.getMarkupLanguage());
         expandTemplet(templet, vars, writer);

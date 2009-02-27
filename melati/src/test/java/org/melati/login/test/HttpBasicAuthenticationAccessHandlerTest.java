22c6bcde93a672b7a6fd44771cfb617bfbd3e436
Tim Pizey <timp@paneris.org>
2009-02-27 16:23:14 +0000

    Lose the dynamic mocks

--- melati/src/test/java/org/melati/login/test/HttpBasicAuthenticationAccessHandlerTest.java
+++ melati/src/test/java/org/melati/login/test/HttpBasicAuthenticationAccessHandlerTest.java
@@ -4,20 +4,10 @@
 package org.melati.login.test;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.melati.login.HttpBasicAuthenticationAccessHandler;
-import org.melati.poem.util.EmptyEnumeration;
-import org.melati.util.HttpServletRequestParameters;
+import org.melati.servlet.test.MockServletRequest;
+import org.melati.servlet.test.MockServletResponse;
 
-import com.mockobjects.constraint.Constraint;
-import com.mockobjects.constraint.IsAnything;
-import com.mockobjects.constraint.IsEqual;
-import com.mockobjects.constraint.IsInstanceOf;
-import com.mockobjects.dynamic.Mock;
-import com.mockobjects.dynamic.OrderedMock;
 
 /**
  * @author timp
@@ -34,63 +24,10 @@ public class HttpBasicAuthenticationAccessHandlerTest extends AccessHandlerTestA
 
   public void setUp() throws Exception {
     super.setUp();
-    Mock mockSession = new Mock(HttpSession.class);
-    mockSession.expectAndReturn("getId", null);
-    mockSession.expectAndReturn("getId", null);
-    mockSession.expectAndReturn("getId", null);
-    Mock mockHttpServletRequest = new Mock(HttpServletRequest.class);
-    mockHttpServletRequest.expectAndReturn( "getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration());
-    mockHttpServletRequest.expectAndReturn("getHeader", "Authorization", null);
-    mockSession.expect("setAttribute", new Constraint []  {new IsEqual("org.melati.login.Login.triggeringRequestParameters"),
-        new IsInstanceOf(HttpServletRequestParameters.class)});
-    mockSession.expect("setAttribute", new Constraint []  {new IsEqual("org.melati.login.Login.triggeringException"),
-        new IsInstanceOf(org.melati.poem.AccessPoemException.class)});
-    mockSession.expect("getAttribute", "org.melati.login.HttpSessionAccessHandler.overlayParameters");
-    mockSession.expect("getAttribute", "org.melati.login.HttpSessionAccessHandler.user");
-
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getCookies", null);
-    Mock mockHttpServletResponse = new OrderedMock(HttpServletResponse.class, "Response with non-default name");
-    //mockHttpServletResponse.expect("sendRedirect",new IsInstanceOf(String.class));
-    //mockSession.expectAndReturn("getAttribute", "org.melati.login.HttpSessionAccessHandler.overlayParameters",
-    //    new HttpServletRequestParameters((HttpServletRequest)mockHttpServletRequest.proxy()));
-
-    mockSession.expectAndReturn("setAttribute", "org.melati.login.Login.triggeringRequestParameters",
-        new HttpServletRequestParameters((HttpServletRequest)mockHttpServletRequest.proxy()));
-    mockHttpServletResponse.expect("setHeader", new Constraint[] {new IsEqual("WWW-Authenticate"), new IsEqual("Basic realm=\"melati\"")});
-    mockHttpServletResponse.expect("sendError", new Constraint[] {new IsAnything(), 
-                       new IsEqual("You need the capability Cool but your access token _guest_ doesn't confer it")});
-    m.setRequest((HttpServletRequest)mockHttpServletRequest.proxy());
-    m.setResponse((HttpServletResponse)mockHttpServletResponse.proxy());
+    MockServletRequest mockHttpServletRequest = new MockServletRequest();
+    MockServletResponse mockHttpServletResponse = new MockServletResponse();
+    m.setRequest(mockHttpServletRequest);
+    m.setResponse(mockHttpServletResponse);
   }
 
   /**
--- melati/src/test/java/org/melati/login/test/HttpSessionAccessHandlerTest.java
+++ melati/src/test/java/org/melati/login/test/HttpSessionAccessHandlerTest.java
@@ -4,19 +4,11 @@
 package org.melati.login.test;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.melati.login.HttpSessionAccessHandler;
-import org.melati.poem.util.EmptyEnumeration;
-import org.melati.util.HttpServletRequestParameters;
+import org.melati.servlet.test.MockServletRequest;
+import org.melati.servlet.test.MockServletResponse;
 
-import com.mockobjects.constraint.Constraint;
-import com.mockobjects.constraint.IsEqual;
-import com.mockobjects.constraint.IsInstanceOf;
-import com.mockobjects.dynamic.Mock;
-import com.mockobjects.dynamic.OrderedMock;
 
 /**
  * @author timp
@@ -33,59 +25,11 @@ public class HttpSessionAccessHandlerTest extends AccessHandlerTestAbstract {
 
   public void setUp() throws Exception {
     super.setUp();
-    Mock mockSession = new Mock(HttpSession.class);
-    mockSession.expectAndReturn("getId", null); 
-    mockSession.expectAndReturn("getId", null); 
-    mockSession.expectAndReturn("getId", null); 
-    Mock mockHttpServletRequest = new Mock(HttpServletRequest.class); 
-    mockHttpServletRequest.expectAndReturn( "getParameterNames", new EmptyEnumeration()); 
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration()); 
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration()); 
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration()); 
-    mockHttpServletRequest.expectAndReturn("getContextPath", null);
-    mockHttpServletRequest.expectAndReturn("getServletPath", null);
-    mockHttpServletRequest.expectAndReturn("getPathInfo", null);
-    mockHttpServletRequest.expectAndReturn("getQueryString", null);
-    mockHttpServletRequest.expectAndReturn("getMethod", null);
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration()); 
-    mockHttpServletRequest.expectAndReturn("getParameterNames", new EmptyEnumeration()); 
-    mockSession.expect("setAttribute", new Constraint []  {new IsEqual("org.melati.login.Login.triggeringRequestParameters"), 
-        new IsInstanceOf(HttpServletRequestParameters.class)}); 
-    mockSession.expect("setAttribute", new Constraint []  {new IsEqual("org.melati.login.Login.triggeringException"), 
-        new IsInstanceOf(org.melati.poem.AccessPoemException.class)}); 
-    mockSession.expect("getAttribute", "org.melati.login.HttpSessionAccessHandler.overlayParameters"); 
-    mockSession.expect("getAttribute", "org.melati.login.HttpSessionAccessHandler.user"); 
+    MockServletRequest mockHttpServletRequest = new MockServletRequest(); 
     
-    mockHttpServletRequest.expectAndReturn("getSession", Boolean.TRUE, mockSession.proxy());
-    mockHttpServletRequest.expectAndReturn("getCookies", null);
-    Mock mockHttpServletResponse = new OrderedMock(HttpServletResponse.class, "Response with non-default name"); 
-    mockHttpServletResponse.expect("sendRedirect",new IsInstanceOf(String.class));
-    //mockSession.expectAndReturn("getAttribute", "org.melati.login.HttpSessionAccessHandler.overlayParameters", 
-    //    new HttpServletRequestParameters((HttpServletRequest)mockHttpServletRequest.proxy())); 
-    
-    mockSession.expectAndReturn("setAttribute", "org.melati.login.Login.triggeringRequestParameters", 
-        new HttpServletRequestParameters((HttpServletRequest)mockHttpServletRequest.proxy())); 
-    m.setRequest((HttpServletRequest)mockHttpServletRequest.proxy());
-    m.setResponse((HttpServletResponse)mockHttpServletResponse.proxy());
+    MockServletResponse mockHttpServletResponse = new MockServletResponse(); 
+    m.setRequest(mockHttpServletRequest);
+    m.setResponse(mockHttpServletResponse);
   }
 
   /**

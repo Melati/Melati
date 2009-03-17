b958fcfba688f56e81691fc2e65769455a9f2969
Tim Pizey <timp@paneris.org>
2009-03-17 21:38:00 +0000

    Set context as well

--- melati-archetype/src/main/resources/archetype-resources/src/test/java/melati/test/JettyWebTestCase.java
+++ melati-archetype/src/main/resources/archetype-resources/src/test/java/melati/test/JettyWebTestCase.java
@@ -63,6 +63,7 @@ public class #capitalise(${artifactId})JettyWebTestCase extends JettyWebTestCase
   public #capitalise(${artifactId})JettyWebTestCase(String name) {
     super(name);
     webAppDirName = "src/main/webapp";
+    contextName = "";
   }
 
   /**

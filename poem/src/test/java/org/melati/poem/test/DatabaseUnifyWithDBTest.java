37572176647df2e4e20382e3fccbdc46d60ea5a7
Tim Pizey <timp@paneris.org>
2011-06-09 22:18:15 +0000

    Failing test

--- poem/src/test/java/org/melati/poem/test/DatabaseUnifyWithDBTest.java
+++ poem/src/test/java/org/melati/poem/test/DatabaseUnifyWithDBTest.java
@@ -129,4 +129,85 @@ public class DatabaseUnifyWithDBTest extends TestCase {
     assertEquals("testable", db.getTable("testable").getName());
     db.setLogSQL(false);
   }
+
+  /**
+   * Set up tables not in dsd to exercise other branch of unification.
+   */
+  public void testUnifyWithDB_nonIDKey() throws Exception {
+    Driver driver = null;
+    Class<?> driverClass;
+    String url = "jdbc:hsqldb:mem:unifiable";
+    driverClass = Class.forName("org.hsqldb.jdbcDriver");
+    driver = (Driver)driverClass.newInstance();
+    Properties info = new Properties();
+    info.put("user", "sa");
+    info.put("password", "");
+
+    Connection c = driver.connect(url, info);
+    Statement s = c.createStatement();
+    Hsqldb dbms = new Hsqldb();
+    StringBuffer sqb = new StringBuffer();
+    sqb.append("CREATE TABLE " + dbms.getQuotedName("testable2") + " (");
+    sqb.append(dbms.getQuotedName("testableId") + " INTEGER NOT NULL");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("testname") + " VARCHAR(233) NOT NULL");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("deleted") + " BOOLEAN");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("canRead") + " INTEGER");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("canCreate") + " INTEGER");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("canDelete") + " INTEGER");
+    sqb.append(", ");
+    sqb.append(dbms.getQuotedName("canSelect") + " INTEGER");
+    sqb.append(")");
+
+    try { 
+      s.executeUpdate(sqb.toString());
+    } catch (SQLException e) { 
+      try { 
+        assertTrue(e.getMessage().indexOf("already exists") > 0);
+      } catch (Error e2) { 
+        throw e;
+      }
+    }
+    try { 
+      s.executeUpdate("CREATE UNIQUE INDEX \"TESTABLE_ID_INDEX\" ON \"TESTABLE2\" (\"TESTABLEID\")");    
+    } catch (SQLException e) { 
+      assertTrue(e.getMessage(),e.getMessage().indexOf("already exists") > 0);
+    }
+    StringBuffer sqb2 = new StringBuffer();
+    sqb2.append("INSERT INTO " + dbms.getQuotedName("testable2") + " (");
+    sqb2.append(dbms.getQuotedName("id")) ;
+    sqb2.append(", ");
+    sqb2.append(dbms.getQuotedName("testname"))  ;
+    sqb2.append(") VALUES (");
+    sqb2.append("0, 't1'");
+    sqb2.append(") ");
+
+    try { 
+      s.executeUpdate(sqb2.toString());
+    } catch (SQLException e) { 
+      assertTrue(e.getMessage(),e.getMessage().indexOf("Violation of unique index") >= 0);
+    }
+   
+    s.close();
+    c.commit();
+    if (!c.isClosed()) {
+      Statement st = c.createStatement();
+      //st.execute("SHUTDOWN SCRIPT"); 
+      st.close();
+    }
+    c.close();
+    PoemDatabase db = new PoemDatabase();
+    //db.setLogSQL(true);
+    db.connect("unifiable", "org.melati.poem.dbms.Hsqldb",
+        url, "sa", "", 22);
+    assertEquals(22, db.getFreeTransactionsCount());
+    assertTrue(db.getClass().getName() == "org.melati.poem.PoemDatabase");
+    assertEquals("testable2", db.getTable("testable2").getName());
+    db.setLogSQL(false);
+  }
+
 }

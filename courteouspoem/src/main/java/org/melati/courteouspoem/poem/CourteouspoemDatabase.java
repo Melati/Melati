dede3e927fd1d2f95511e5a7ffd9951e8712731f
Tim Pizey <timp@paneris.org>
2011-06-19 23:17:30 +0000

    Drop in sensible order

--- courteouspoem/src/main/java/org/melati/courteouspoem/poem/CourteouspoemDatabase.java
+++ courteouspoem/src/main/java/org/melati/courteouspoem/poem/CourteouspoemDatabase.java
@@ -15,13 +15,13 @@ public class CourteouspoemDatabase extends CourteouspoemDatabaseBase
   public void disconnect() throws PoemException {
     beginStructuralModification();
     try { 
-      modifyStructure("DROP TABLE " + getTableCategoryTable().quotedName());
       modifyStructure("DROP TABLE " + getGroupMembershipTable().quotedName());
       modifyStructure("DROP TABLE " + getGroupCapabilityTable().quotedName());
       modifyStructure("DROP TABLE " + getGroupTable().quotedName());
       modifyStructure("DROP TABLE " + getCapabilityTable().quotedName());
       modifyStructure("DROP TABLE " + getColumnInfoTable().quotedName());
       modifyStructure("DROP TABLE " + getTableInfoTable().quotedName());
+      modifyStructure("DROP TABLE " + getTableCategoryTable().quotedName());
       modifyStructure("DROP TABLE " + getUserTable().quotedName());
       modifyStructure("DROP TABLE " + getSettingTable().quotedName());
     }

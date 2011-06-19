6dc55476956d025630c188412550b29fe7594613
Tim Pizey <timp@paneris.org>
2011-06-19 23:23:48 +0000

    Order of dropping

--- courteouspoem/src/main/java/org/melati/courteouspoem/poem/CourteouspoemDatabase.java
+++ courteouspoem/src/main/java/org/melati/courteouspoem/poem/CourteouspoemDatabase.java
@@ -18,9 +18,9 @@ public class CourteouspoemDatabase extends CourteouspoemDatabaseBase
       modifyStructure("DROP TABLE " + getGroupMembershipTable().quotedName());
       modifyStructure("DROP TABLE " + getGroupCapabilityTable().quotedName());
       modifyStructure("DROP TABLE " + getGroupTable().quotedName());
-      modifyStructure("DROP TABLE " + getCapabilityTable().quotedName());
       modifyStructure("DROP TABLE " + getColumnInfoTable().quotedName());
       modifyStructure("DROP TABLE " + getTableInfoTable().quotedName());
+      modifyStructure("DROP TABLE " + getCapabilityTable().quotedName());
       modifyStructure("DROP TABLE " + getTableCategoryTable().quotedName());
       modifyStructure("DROP TABLE " + getUserTable().quotedName());
       modifyStructure("DROP TABLE " + getSettingTable().quotedName());

8f03aefe5a1aa3f42dd111270989fd5e241d87b0
Tim Pizey <timp@paneris.org>
2011-07-21 07:04:35 +0000

    Do all

---
+++ regenerate.sh
@@ -0,0 +1,8 @@
+rm courteouspoem/src/main/java/org/melati/courteouspoem/poem/generated/*.java
+rm melati/src/test/java/org/melati/admin/test/generated/*.java
+rm melati/src/test/java/org/melati/util/test/generated/*.java
+rm melati-example-contacts/src/main/java/org/melati/example/contacts/generated/*.java
+rm melati-example-odmg/src/main/java/org/melati/example/odmg/generated/*.java
+rm poem/src/main/java/org/melati/poem/generated/*.java
+rm poem/src/test/java/org/melati/poem/test/generated/*.java
+mvn install

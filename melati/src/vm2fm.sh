ae2ddb21886821dba79420d4a25bf4dc56f0a0ba
Tim Pizey <timp@paneris.org>
2012-03-08 11:09:01 +0000

    Convert velocity templates to Freemarker

---
+++ melati/src/vm2fm.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# vm2fm.jar is a binaryu blob downloaded from 
+# http://freemarker.sourceforge.net/usCavalry.html
+
+
+ ./vm2fm.jar main/java/org/melati/admin/Add.vm main/java/org/melati/admin/Add.fml
+ 
+ ./vm2fm.jar main/java/org/melati/admin/AnticipatedException.vm main/java/org/melati/admin/AnticipatedException.fml
+ ./vm2fm.jar main/java/org/melati/admin/blank.vm main/java/org/melati/admin/blank.fml
+ ./vm2fm.jar main/java/org/melati/admin/Bottom.vm main/java/org/melati/admin/Bottom.fml
+ ./vm2fm.jar main/java/org/melati/admin/CSV.vm main/java/org/melati/admin/CSV.fml
+ ./vm2fm.jar main/java/org/melati/admin/DeleteFailure.vm main/java/org/melati/admin/DeleteFailure.fml
+ ./vm2fm.jar main/java/org/melati/admin/Display.vm main/java/org/melati/admin/Display.fml
+ ./vm2fm.jar main/java/org/melati/admin/DSD.vm main/java/org/melati/admin/DSD.fml
+ ./vm2fm.jar main/java/org/melati/admin/EditHeader.vm main/java/org/melati/admin/EditHeader.fml
+ ./vm2fm.jar main/java/org/melati/admin/Edit.vm main/java/org/melati/admin/Edit.fml
+ ./vm2fm.jar main/java/org/melati/admin/Main.vm main/java/org/melati/admin/Main.fml
+ ./vm2fm.jar main/java/org/melati/admin/Navigation.vm main/java/org/melati/admin/Navigation.fml
+ ./vm2fm.jar main/java/org/melati/admin/PopupSelect.vm main/java/org/melati/admin/PopupSelect.fml
+ ./vm2fm.jar main/java/org/melati/admin/PrimarySelect.vm main/java/org/melati/admin/PrimarySelect.fml
+ ./vm2fm.jar main/java/org/melati/admin/Record.vm main/java/org/melati/admin/Record.fml
+ ./vm2fm.jar main/java/org/melati/admin/Selection.vm main/java/org/melati/admin/Selection.fml
+ ./vm2fm.jar main/java/org/melati/admin/SelectionWindow.vm main/java/org/melati/admin/SelectionWindow.fml
+ ./vm2fm.jar main/java/org/melati/admin/Status.vm main/java/org/melati/admin/Status.fml
+ ./vm2fm.jar main/java/org/melati/admin/Table.vm main/java/org/melati/admin/Table.fml
+ ./vm2fm.jar main/java/org/melati/admin/Top.vm main/java/org/melati/admin/Top.fml
+ ./vm2fm.jar main/java/org/melati/admin/Tree.vm main/java/org/melati/admin/Tree.fml
+ ./vm2fm.jar main/java/org/melati/admin/Updated.vm main/java/org/melati/admin/Updated.fml
+ ./vm2fm.jar main/java/org/melati/admin/UploadDone.vm main/java/org/melati/admin/UploadDone.fml
+ ./vm2fm.jar main/java/org/melati/admin/Upload.vm main/java/org/melati/admin/Upload.fml
+ ./vm2fm.jar main/java/org/melati/app/TemplateApp.vm main/java/org/melati/app/TemplateApp.fml
+ ./vm2fm.jar main/java/org/melati/login/LoginFailure.vm main/java/org/melati/login/LoginFailure.fml
+ ./vm2fm.jar main/java/org/melati/login/LoginSuccess.vm main/java/org/melati/login/LoginSuccess.fml
+ ./vm2fm.jar main/java/org/melati/login/Login.vm main/java/org/melati/login/Login.fml
+ ./vm2fm.jar main/java/org/melati/login/Logout.vm main/java/org/melati/login/Logout.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/error/java.lang.Exception.vm main/java/org/melati/templets/html/error/java.lang.Exception.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/java.lang.Exception.vm main/java/org/melati/templets/html/java.lang.Exception.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/java.lang.Object.vm main/java/org/melati/templets/html/java.lang.Object.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.AccessPoemException.vm main/java/org/melati/templets/html/org.melati.poem.AccessPoemException.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.BooleanPoemType.vm main/java/org/melati/templets/html/org.melati.poem.BooleanPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.DatePoemType-dropdown.vm main/java/org/melati/templets/html/org.melati.poem.DatePoemType-dropdown.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.DatePoemType-monthyeardropdown.vm main/java/org/melati/templets/html/org.melati.poem.DatePoemType-monthyeardropdown.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.DatePoemType.vm main/java/org/melati/templets/html/org.melati.poem.DatePoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.IndexPoemType.vm main/java/org/melati/templets/html/org.melati.poem.IndexPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.IntegerPoemType.vm main/java/org/melati/templets/html/org.melati.poem.IntegerPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.JdbcPersistent.vm main/java/org/melati/templets/html/org.melati.poem.JdbcPersistent.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.PasswordPoemType.vm main/java/org/melati/templets/html/org.melati.poem.PasswordPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.ReferencePoemType-SelectionWindow.vm main/java/org/melati/templets/html/org.melati.poem.ReferencePoemType-SelectionWindow.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.StringPoemType-UploadURL.vm main/java/org/melati/templets/html/org.melati.poem.StringPoemType-UploadURL.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.StringPoemType.vm main/java/org/melati/templets/html/org.melati.poem.StringPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.poem.TimestampPoemType-dropdown.vm main/java/org/melati/templets/html/org.melati.poem.TimestampPoemType-dropdown.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.util.JSStaticTree.vm main/java/org/melati/templets/html/org.melati.util.JSStaticTree.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/org.melati.util.TreeNode.vm main/java/org/melati/templets/html/org.melati.util.TreeNode.fml
+ ./vm2fm.jar main/java/org/melati/templets/html/passwordForChecking.vm main/java/org/melati/templets/html/passwordForChecking.fml
+ ./vm2fm.jar main/java/org/melati/templets/wml/java.lang.Exception.vm main/java/org/melati/templets/wml/java.lang.Exception.fml
+ ./vm2fm.jar main/java/org/melati/templets/wml/java.lang.Object.vm main/java/org/melati/templets/wml/java.lang.Object.fml
+ ./vm2fm.jar main/java/org/melati/templets/wml/org.melati.poem.IndexPoemType.vm main/java/org/melati/templets/wml/org.melati.poem.IndexPoemType.fml
+ ./vm2fm.jar main/java/org/melati/templets/wml/org.melati.poem.JdbcPersistent.vm main/java/org/melati/templets/wml/org.melati.poem.JdbcPersistent.fml
+ ./vm2fm.jar main/java/org/melati/templets/wml/org.melati.poem.StringPoemType.vm main/java/org/melati/templets/wml/org.melati.poem.StringPoemType.fml
+ ./vm2fm.jar main/java/org/melati/test/CharsetTemplateServletTest.vm main/java/org/melati/test/CharsetTemplateServletTest.fml
+ ./vm2fm.jar main/java/org/melati/test/EmailTemplateServletTest.vm main/java/org/melati/test/EmailTemplateServletTest.fml
+ ./vm2fm.jar main/java/org/melati/test/Email.vm main/java/org/melati/test/Email.fml
+ ./vm2fm.jar main/java/org/melati/test/TemplateServletTest.vm main/java/org/melati/test/TemplateServletTest.fml
+ ./vm2fm.jar main/java/org/melati/test/WebmacroMelatiServletTest.vm main/java/org/melati/test/WebmacroMelatiServletTest.fml
+ ./vm2fm.jar test/java/org/melati/admin/test/ProxyCaller.vm test/java/org/melati/admin/test/ProxyCaller.fml
+ ./vm2fm.jar test/java/org/melati/admin/test/Specialised.vm test/java/org/melati/admin/test/Specialised.fml
+ ./vm2fm.jar test/java/org/melati/admin/test/UploadedFile-specials.vm test/java/org/melati/admin/test/UploadedFile-specials.fml
+ ./vm2fm.jar test/java/org/melati/servlet/test/ClasspathRenderedException.vm test/java/org/melati/servlet/test/ClasspathRenderedException.fml
+# ./vm2fm.jar test/java/org/melati/template/test/Templated.vm test/java/org/melati/template/test/TemplatedWithSyntaxError.vm test/java/org/melati/template/test/TemplatedWithSyntaxError.fml
+# ./vm2fm.jar test/java/org/melati/template/test/TemplatedWithWMSyntaxError.vm test/java/org/melati/template/test/TemplatedWithWMSyntaxError.fml
+ ./vm2fm.jar test/java/org/melati/template/test/Templated.vm test/java/org/melati/template/test/Templated.fml

30d3c0080c9d54231927ff6b100f55d071d8d846
Tim Pizey <timp@paneris.org>
2012-11-14 15:30:58 +0000

    Unused imports

--- melati/src/test/java/org/melati/template/test/JSONMarkupLanguageSpec.java
+++ melati/src/test/java/org/melati/template/test/JSONMarkupLanguageSpec.java
@@ -1,37 +1,24 @@
 package org.melati.template.test;
 
 import org.melati.poem.PoemLocale;
-import org.melati.template.AttributeMarkupLanguage;
 import org.melati.template.ClassNameTempletLoader;
-import org.melati.template.HTMLAttributeMarkupLanguage;
-import org.melati.template.HTMLMarkupLanguage;
 import org.melati.template.JSONMarkupLanguage;
 
 /**
  * @author timp
- * @since 2 Jul 2007
+ * @since 2007-11-07
  *
  */
 public abstract class JSONMarkupLanguageSpec extends MarkupLanguageSpec {
 
-  /**
-   * @param arg0
-   */
   public JSONMarkupLanguageSpec(String arg0) {
     super(arg0);
   }
 
-  /**
-   * 
-   */
   public JSONMarkupLanguageSpec() {
     super();
   }
 
-  /** 
-   * {@inheritDoc}
-   * @see org.melati.template.test.MarkupLanguageSpec#setUp()
-   */
   protected void setUp() throws Exception {
     super.setUp();
     ml = new JSONMarkupLanguage(

8949af881539d2aa2525951a33174f3aac1bd2c3
Tim Pizey <timp@paneris.org>
2012-11-15 17:14:38 +0000

    Default to string

--- melati/src/main/java/org/melati/admin/PoemGvisTypeConverter.java
+++ melati/src/main/java/org/melati/admin/PoemGvisTypeConverter.java
@@ -3,7 +3,7 @@ package org.melati.admin;
 public class PoemGvisTypeConverter {
 
   public static  String convert(int poemType){
-    if (poemType > 0)
+    if (PoemGvisType.from(poemType) == null) // eg table types 
       return "string";
     else
       return PoemGvisType.from(poemType).gvisJsonTypeName();

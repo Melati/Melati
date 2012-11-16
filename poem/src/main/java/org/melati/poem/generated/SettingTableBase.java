6f7685ade7856ead8ac40e2594cdf67cffb30dbf
Tim Pizey <timp@paneris.org>
2012-11-16 10:47:35 +0000

    Looks like DSD will never get this done

--- poem/src/main/java/org/melati/poem/generated/SettingTableBase.java
+++ poem/src/main/java/org/melati/poem/generated/SettingTableBase.java
@@ -191,6 +191,7 @@ public class SettingTableBase<T extends Setting> extends ValueInfoTable<T> {
             ((Setting)g).setValue((String)cooked);
           }
 
+          @SuppressWarnings("unchecked")
           public Field<String> asField(Persistent g) {
             return ((Setting)g).getValueField();
           }

78a4cfc342c57282815272c940d0af960ca92f3c
Tim Pizey <timp@paneris.org>
2013-07-07 22:56:08 +0000

    New methods in jdk7 to support jdbc4.1

---
+++ throwing-jdbc/src/jdbc4.1/java/org/melati/poem/dbms/test/sql/ThrowingConnectionJdbc4_1.java
@@ -0,0 +1,99 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2013 Tim Pizey
+ *
+ * Part of Melati (http://melati.org), a framework for the rapid
+ * development of clean, maintainable web applications.
+ *
+ * Melati is free software; Permission is granted to copy, distribute
+ * and/or modify this software under the terms either:
+ *
+ * a) the GNU General Public License as published by the Free Software
+ *    Foundation; either version 2 of the License, or (at your option)
+ *    any later version,
+ *
+ *    or
+ *
+ * b) any version of the Melati Software License, as published
+ *    at http://melati.org
+ *
+ * You should have received a copy of the GNU General Public License and
+ * the Melati Software License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
+ * GNU General Public License and visit http://melati.org to obtain the
+ * Melati Software License.
+ *
+ * Feel free to contact the Developers of Melati (http://melati.org),
+ * if you would like to work out a different arrangement than the options
+ * outlined here.  It is our intention to allow Melati to be used by as
+ * wide an audience as possible.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Tim Pizey <timp At paneris.org>
+ *     http://paneris.org/~timp
+ */
+
+package org.melati.poem.dbms.test.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.Executor;
+
+/**
+ * A JDBC4.1 {@link Connection}, 
+ * decorated to throw an SQLException on command.
+ * 
+ * @author timp
+ * @since  2013/07/07
+ *
+ */
+public abstract class ThrowingConnectionJdbc4_1 
+    extends ThrowingConnectionJdbc4 
+    implements Connection {
+ 
+ @Override
+ public void setSchema(String schema) throws SQLException {
+    if (shouldThrow(this.getClass().getInterfaces()[0], "setSchema"))
+      throw new SQLException("Connection.setSchema() bombed");
+    it.setSchema(schema);
+ }
+
+ @Override
+ public String getSchema() throws SQLException {
+    if (shouldThrow(this.getClass().getInterfaces()[0], "getSchema"))
+      throw new SQLException("Connection.getSchema() bombed");
+    return it.getSchema();
+ }
+
+ @Override
+ public void abort(Executor executor) throws SQLException {
+    if (shouldThrow(this.getClass().getInterfaces()[0], "abort"))
+      throw new SQLException("Connection.abort() bombed");
+    it.abort(executor);
+ }
+
+ @Override
+ public void setNetworkTimeout(Executor executor, int milliseconds)
+   throws SQLException {
+    if (shouldThrow(this.getClass().getInterfaces()[0], "setNetworkTimeout"))
+      throw new SQLException("Connection.setNetworkTimeout() bombed");
+    it.setNetworkTimeout(executor, milliseconds);
+ }
+
+ @Override
+ public int getNetworkTimeout() throws SQLException {
+    if (shouldThrow(this.getClass().getInterfaces()[0], "getNetworkTimeout"))
+      throw new SQLException("Connection.getNetworkTimeout() bombed");
+    return it.getNetworkTimeout();
+ }
+
+}

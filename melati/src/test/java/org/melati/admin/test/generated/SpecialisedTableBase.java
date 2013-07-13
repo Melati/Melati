f980a3c17549de56ba0d2aebc7d0cf2dcf2171d0
Tim Pizey <timp@paneris.org>
2013-07-13 23:16:59 +0000

    Regenerated: additional javadoc

--- melati/src/test/java/org/melati/admin/test/generated/SpecialisedTableBase.java
+++ melati/src/test/java/org/melati/admin/test/generated/SpecialisedTableBase.java
@@ -59,6 +59,12 @@ public class SpecialisedTableBase<T extends Specialised> extends AdminTestTable<
     return (AdminTestDatabaseTables)getDatabase();
   }
 
+
+ /**
+  * Initialise this table by defining its columns.
+  *
+  * see org.melati.poem.prepro.TableDef#generateTableBaseJava 
+  */
   public void init() throws PoemException {
     super.init();
     defineColumn(col_id =
